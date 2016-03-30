4fde7cce0a8f69ce9345320d02fcae1910fc7ec3
Jamaz <jamazruan@rainwings.cn>
2016-03-30 15:30:47 +0800

    bugfix:增加好友、竞技场红点 开放等级的判断

--- server/FSApp/src/main/java/com/rw/service/redpoint/impl/FriendCollector.java
+++ server/FSApp/src/main/java/com/rw/service/redpoint/impl/FriendCollector.java
@@ -7,12 +7,18 @@ import java.util.Map;
 import com.playerdata.FriendMgr;
 import com.playerdata.Player;
 import com.rw.service.redpoint.RedPointType;
+import com.rwbase.dao.openLevelLimit.CfgOpenLevelLimitDAO;
+import com.rwbase.dao.openLevelLimit.eOpenLevelType;
+import com.rwbase.dao.openLevelLimit.pojo.CfgOpenLevelLimit;
 
-public class FriendCollector implements RedPointCollector{
+public class FriendCollector implements RedPointCollector {
 
 	@Override
 	public void fillRedPoints(Player player, Map<RedPointType, List<String>> map) {
 		FriendMgr friendMgr = player.getFriendMgr();
+		if (!CfgOpenLevelLimitDAO.getInstance().isOpen(eOpenLevelType.FRIEND, player.getLevel())) {
+			return;
+		}
 		if (friendMgr.hasReceivePower()) {
 			map.put(RedPointType.FRIEND_WINDOW_RECEIVE_POWER, Collections.EMPTY_LIST);
 		}
