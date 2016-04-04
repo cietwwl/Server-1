2f874fb3505d08ecab20316651fcd50045f13d76
HC <huchong@rainwings.cn>
2016-04-04 17:37:16 +0800

    bug fix：2049 PVE模块，万仙阵在凌晨5点后没有刷新重置次数

--- server/FSApp/src/main/java/com/rwbase/dao/battletower/pojo/BossCacheInfo.java
+++ server/FSApp/src/main/java/com/rwbase/dao/battletower/pojo/BossCacheInfo.java
@@ -6,8 +6,11 @@ package com.rwbase.dao.battletower.pojo;
  * @Description 产生Boss的信息（用于还没重置的时候的缓存数据）
  */
 public class BossCacheInfo {
-	private final int bossId;// 产生的BossId
-	private final int markId;// Boss产生所在的里程碑
+	private int bossId;// 产生的BossId
+	private int markId;// Boss产生所在的里程碑
+
+	public BossCacheInfo() {
+	}
 
 	public BossCacheInfo(int bossId, int markId) {
 		this.bossId = bossId;
