2ae9d63b691e2ad52f5777354c33f78abc22bfe4
Jamaz <jamazruan@rainwings.cn>
2016-04-07 18:45:15 +0800

    bugfix:修复offset与limit偏移误差

--- server/FSApp/src/main/java/com/gm/task/GmGetRankList.java
+++ server/FSApp/src/main/java/com/gm/task/GmGetRankList.java
@@ -126,6 +126,10 @@ public class GmGetRankList implements IGmTask {
 		if (dbString == null) {
 			return Collections.EMPTY_LIST; 
 		}
+		if(offset <= 0){
+			GameLog.error("GmGetRankingList", "#getRankList()", "获取排行榜活动offset异常：offset=" + offset + ",limit=" + limit  + ",rankType=" + rankType);
+			return Collections.EMPTY_LIST;
+		}
 		ArrayList<String> list = new ArrayList<String>();
 		try {
 			JSONArray array = new JSONArray(dbString);
@@ -134,9 +138,10 @@ public class GmGetRankList implements IGmTask {
 				GameLog.error("GmGetRankingList", "#getRankList()", "获取排行榜活动记录异常：offset=" + offset + ",limit=" + limit + ",len=" + len + ",rankType=" + rankType);
 				return list;
 			}
-			int max = offset + limit;
+			int start = offset - 1;
+			int max = start + limit;
 			len = Math.min(max, len);
-			for (int i = offset; i < len; i++) {
+			for (int i = start; i < len; i++) {
 				list.add(array.getString(i));
 			}
 		} catch (Exception e) {
