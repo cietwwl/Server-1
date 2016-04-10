e3f56e9dc351d6b4f409b087d0fc0ffc024d4dfe
Jamaz <jamazruan@rainwings.cn>
2016-04-10 21:14:18 +0800

    bugfix:修复竞技场结算发送邮件时间不准确的问题 bug#2078

--- server/FSApp/src/main/java/com/rwbase/gameworld/GameWorldConstant.java
+++ server/FSApp/src/main/java/com/rwbase/gameworld/GameWorldConstant.java
@@ -6,16 +6,29 @@ public interface GameWorldConstant {
 	 * 重置时间：小时
 	 */
 	public static final int RESET_HOUR = 5;
-	
 
 	/**
 	 * 重置时间：分钟
 	 */
 	public static final int RESET_MINUTE = 0;
-	
 
 	/**
 	 * 重置时间：秒
 	 */
 	public static final int RESET_SECOND = 0;
+
+	/**
+	 * 竞技场结算小时
+	 */
+	public static final int ARENA_CALCULATE_HOUR = 21;
+	/**
+	 * 竞技场结算分钟
+	 */
+	public static final int ARENA_CALCULATE_MINUTE = 0;
+	/**
+	 * 竞技场结算秒
+	 */
+	public static final int ARENA_CALCULATE_SECOND = 0;
 }
+
+
