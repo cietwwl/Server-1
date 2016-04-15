1a5008abe896de1317f7d938139896d45629b3e6
HC <huchong@rainwings.cn>
2016-04-15 15:36:23 +0800

    bug fix:320 帮派模块，玩家退出帮派后，点击申请加入时显示下次可以加入的具体时间

--- server/FSApp/src/main/java/com/rw/service/group/GroupBaseManagerHandler.java
+++ server/FSApp/src/main/java/com/rw/service/group/GroupBaseManagerHandler.java
@@ -48,6 +48,7 @@ import com.rwproto.GroupCommonProto.RequestType;
  * @Description 帮派的基础处理
  */
 public class GroupBaseManagerHandler {
+	private static final String QUIT_GROUP_TIME_TIP_FOR_CREATE = "%s后才可创建帮派";
 	private static GroupBaseManagerHandler handler;
 
 	public static GroupBaseManagerHandler getHandler() {
@@ -105,8 +106,9 @@ public class GroupBaseManagerHandler {
 		long now = System.currentTimeMillis();
 		// 检查冷却时间
 		long quitGroupTime = baseData.getQuitGroupTime();
-		if (quitGroupTime > 0 && (now - quitGroupTime) < TimeUnit.SECONDS.toMillis(gbct.getJoinGroupCoolingTime())) {
-			return GroupCmdHelper.groupBaseMgrFillFailMsg(commonRsp, "距离上次退出帮派时间太短");
+		long needCoolingTime = TimeUnit.SECONDS.toMillis(gbct.getJoinGroupCoolingTime());
+		if (quitGroupTime > 0 && (now - quitGroupTime) < needCoolingTime) {
+			return GroupCmdHelper.groupBaseMgrFillFailMsg(commonRsp, String.format(QUIT_GROUP_TIME_TIP_FOR_CREATE, GroupUtils.quitGroupTimeTip(now, quitGroupTime, needCoolingTime)));
 		}
 
 		// 检查金钱足不足够
