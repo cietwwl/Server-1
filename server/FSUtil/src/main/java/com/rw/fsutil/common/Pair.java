40c3f80391dc0552545daf91b55aef86fa85705c
franky <franky@rainwings.cn>
2016-04-30 15:23:35 +0800

    bug fix: 忘记提交计算hash值和equal

--- server/FSUtil/src/main/java/com/rw/fsutil/common/Pair.java
+++ server/FSUtil/src/main/java/com/rw/fsutil/common/Pair.java
@@ -15,6 +15,37 @@ public class Pair<T1, T2> implements IReadOnlyPair<T1,T2> {
 		this.t2 = t2;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((t1 == null) ? 0 : t1.hashCode());
+		result = prime * result + ((t2 == null) ? 0 : t2.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pair<?, ?> other = (Pair<?, ?>) obj;
+		if (t1 == null) {
+			if (other.t1 != null)
+				return false;
+		} else if (!t1.equals(other.t1))
+			return false;
+		if (t2 == null) {
+			if (other.t2 != null)
+				return false;
+		} else if (!t2.equals(other.t2))
+			return false;
+		return true;
+	}
+
 	protected T1 t1;
 	protected T2 t2;
 
