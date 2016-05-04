adb9297f81e3d6f1f89e4a3073b18a8e32a87560
allen <allen@rainwings.cn>
2016-05-04 14:56:44 +0800

    dev:增加对活动3的基础支持，不影响现有代码

---
+++ server/FSApp/src/main/java/com/playerdata/activity/rateType/ActivityRateTypeEnum.java
@@ -0,0 +1,32 @@
+package com.playerdata.activity.rateType;
+
+import org.apache.commons.lang3.StringUtils;
+
+public enum ActivityRateTypeEnum{	
+	// implements TypeIdentification
+	Month("1");
+	
+	private String cfgId;
+	private ActivityRateTypeEnum(String cfgId){
+		this.cfgId = cfgId;
+	} 
+	
+	public String getCfgId(){
+		return cfgId;
+	}
+	
+	
+	public static ActivityRateTypeEnum getById(String cfgId){
+		ActivityRateTypeEnum target = null;
+		for (ActivityRateTypeEnum enumTmp : values()) {
+			if(StringUtils.equals(cfgId, enumTmp.getCfgId())){
+				target = enumTmp;
+				break;
+			}
+		}	
+		
+		return target;
+	}
+
+	
+}
---
+++ server/FSApp/src/main/java/com/playerdata/activity/rateType/data/ActivityRateTypeItemHolder.java
@@ -0,0 +1,79 @@
+package com.playerdata.activity.rateType.data;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+import com.playerdata.Player;
+import com.playerdata.activity.rateType.ActivityRateTypeEnum;
+import com.playerdata.activity.rateType.ActivityRateTypeHelper;
+import com.playerdata.dataSyn.ClientDataSynMgr;
+import com.rw.fsutil.cacheDao.MapItemStoreCache;
+import com.rw.fsutil.cacheDao.mapItem.MapItemStore;
+import com.rwbase.common.MapItemStoreFactory;
+import com.rwproto.DataSynProtos.eSynOpType;
+import com.rwproto.DataSynProtos.eSynType;
+
+public class ActivityRateTypeItemHolder{
+	
+	private static ActivityRateTypeItemHolder instance = new ActivityRateTypeItemHolder();
+	
+	public static ActivityRateTypeItemHolder getInstance(){
+		return instance;
+	}
+
+	final private eSynType synType = eSynType.ActivityRateType;
+	
+
+	public List<ActivityRateTypeItem> getItemList(String userId)	
+	{
+		
+		List<ActivityRateTypeItem> itemList = new ArrayList<ActivityRateTypeItem>();
+		Enumeration<ActivityRateTypeItem> mapEnum = getItemStore(userId).getEnum();
+		while (mapEnum.hasMoreElements()) {
+			ActivityRateTypeItem item = (ActivityRateTypeItem) mapEnum.nextElement();
+			itemList.add(item);
+		}
+		
+		return itemList;
+	}
+	
+	public void updateItem(Player player, ActivityRateTypeItem item){
+		getItemStore(player.getUserId()).updateItem(item);
+		ClientDataSynMgr.updateData(player, item, synType, eSynOpType.UPDATE_SINGLE);
+	}
+	
+	public ActivityRateTypeItem getItem(String userId, ActivityRateTypeEnum typeEnum){		
+		String itemId = ActivityRateTypeHelper.getItemId(userId, typeEnum);
+		return getItemStore(userId).getItem(itemId);
+	}
+	
+	
+	public boolean addItem(Player player, ActivityRateTypeItem item){
+	
+		boolean addSuccess = getItemStore(player.getUserId()).addItem(item);
+		if(addSuccess){
+			ClientDataSynMgr.updateData(player, item, synType, eSynOpType.ADD_SINGLE);
+		}
+		return addSuccess;
+	}
+	
+	public boolean removeItem(Player player,ActivityRateTypeEnum type){
+		
+		String uidAndId = ActivityRateTypeHelper.getItemId(player.getUserId(), type);
+		boolean addSuccess = getItemStore(player.getUserId()).removeItem(uidAndId);
+		return addSuccess;
+	}
+	
+	public void synAllData(Player player){
+		List<ActivityRateTypeItem> itemList = getItemList(player.getUserId());			
+		ClientDataSynMgr.synDataList(player, itemList, synType, eSynOpType.UPDATE_LIST);
+	}
+
+	
+	private MapItemStore<ActivityRateTypeItem> getItemStore(String userId) {
+		MapItemStoreCache<ActivityRateTypeItem> cache = MapItemStoreFactory.getActivityRateTypeItemCache();
+		return cache.getMapItemStore(userId, ActivityRateTypeItem.class);
+	}
+	
+}
