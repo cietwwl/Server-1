ab01a1a35108d511effda5bf6f6d2d1660371f71
allen <allen@rainwings.cn>
2016-05-04 16:45:05 +0800

    dev:增加对类型3的支持，不影响现有代码

--- server/FSApp/src/main/java/com/playerdata/activity/rateType/ActivityRateTypeEnum.java
+++ server/FSApp/src/main/java/com/playerdata/activity/rateType/ActivityRateTypeEnum.java
@@ -4,7 +4,9 @@ import org.apache.commons.lang3.StringUtils;
 
 public enum ActivityRateTypeEnum{	
 	// implements TypeIdentification
-	Month("1");
+	ELITE_copy_DOUBLE("1"),
+	Normal_copy_DOUBLE("2");
+
 	
 	private String cfgId;
 	private ActivityRateTypeEnum(String cfgId){
