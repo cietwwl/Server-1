9086cd75ea27053aaae8bd3097055b55aba964d0
Jamaz <jamazruan@rainwings.cn>
2016-05-07 14:33:04 +0800

    dev:屏蔽剧情创建时初始化(会导致没有剧情引导)

--- server/FSApp/src/main/java/com/rw/dataaccess/processor/PlotProgressCreator.java
+++ server/FSApp/src/main/java/com/rw/dataaccess/processor/PlotProgressCreator.java
@@ -8,15 +8,15 @@ public class PlotProgressCreator implements DataExtensionCreator<UserPlotProgres
 	@Override
 	public UserPlotProgress create(String userId) {
 		UserPlotProgress userPlotProgress = new UserPlotProgress();
-		userPlotProgress = new UserPlotProgress();
-		userPlotProgress.setUserId(userId);
-		/**
-		 * <pre>
-		 * 序章特殊剧情，当我创建完角色之后，登录数据推送完毕，我就直接把剧情设置一个假想值
-		 * 保证不管角色当前是故意退出游戏跳过剧情，或者是出现意外退出，在下次进来都不会有剧情的重复问题
-		 * </pre>
-		 */
-		userPlotProgress.getProgressMap().putIfAbsent("0", -1);
+//		userPlotProgress = new UserPlotProgress();
+//		userPlotProgress.setUserId(userId);
+//		/**
+//		 * <pre>
+//		 * 序章特殊剧情，当我创建完角色之后，登录数据推送完毕，我就直接把剧情设置一个假想值
+//		 * 保证不管角色当前是故意退出游戏跳过剧情，或者是出现意外退出，在下次进来都不会有剧情的重复问题
+//		 * </pre>
+//		 */
+//		userPlotProgress.getProgressMap().putIfAbsent("0", -1);
 		return userPlotProgress;
 	}
 
