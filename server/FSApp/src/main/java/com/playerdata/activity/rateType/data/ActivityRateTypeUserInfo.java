daffe217221a1198e864945cabb7e9b392b763cd
yxf <xiaofeiyang@rainwings.cn>
2016-05-10 14:20:20 +0800

    5-11 双倍数据

---
+++ server/FSApp/src/main/java/com/playerdata/activity/rateType/data/ActivityRateTypeUserInfo.java
@@ -0,0 +1,17 @@
+package com.playerdata.activity.rateType.data;
+
+import com.rwbase.common.enu.eAttrIdDef;
+
+
+public class ActivityRateTypeUserInfo  {
+
+	private int coin;
+	private int gold;
+
+	private int exp;
+	
+	
+	
+	
+}
+
