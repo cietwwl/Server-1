55ce2894fb905ef0a25e1e13b118d6dc7a322f7e
yxf <xiaofeiyang@rainwings.cn>
2016-05-10 11:24:27 +0800

    5-11 已有修改

--- server/FSChargeWeb/src/main/java/com/fy/ChargeService.java
+++ server/FSChargeWeb/src/main/java/com/fy/ChargeService.java
@@ -1,9 +1,7 @@
 package com.fy;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.URLDecoder;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.http.protocol.HTTP;
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.interceptor.ServletResponseAware;
 
@@ -27,46 +24,62 @@ public class ChargeService extends ActionSupport implements ServletRequestAware,
 
 	private HttpServletResponse response;
 
-	private  HttpServletRequest request;
+	private HttpServletRequest request;
 
-	public  void doService() {
+	public void doService() {
 
-		System.out.println("!!!!!!!!!!!!!!!入口" );
+		
 		try {
 			String jsonContent = receivePost(request);
 			
 			ContentPojo contentPojo = FastJsonUtil.fromJson(jsonContent, ContentPojo.class);
 			
 			ChargeLog.info("charge", contentPojo.getCpTradeNo(), jsonContent);
-			boolean success  = reqGameServer(jsonContent, contentPojo);		
-			String result = success?"0":"-1";
-				PrintWriter writer = response.getWriter();
-				writer.write(result);	
-				writer.flush();
-				writer.close();			
-			} catch (Exception e) {
-				e.printStackTrace();
-			}		
+			boolean success  = reqGameServer(jsonContent, contentPojo);
+			
+			String result = success?"0":"-1";			
+			
+			PrintWriter writer = response.getWriter();
+			
+			writer.write(result);	
+			writer.flush();
+			writer.close();			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	
-	@SuppressWarnings({"deprecation"})
+	@SuppressWarnings("rawtypes")
 	private String receivePost(HttpServletRequest request) throws Exception{
 		
-		BufferedReader br =  new BufferedReader(new InputStreamReader(request.getInputStream()));
-		String line = null;
-		StringBuilder sb = new StringBuilder();
-		while((line = br.readLine())!=null){
-			sb.append(line);
+		Enumeration parameterNames = request.getParameterNames();
+		
+		//无语啊 yh post过来的参数是不带参数名的，只能从名字来获取了。
+		String json = null;
+		while (parameterNames.hasMoreElements()) {
+			json = (String) parameterNames.nextElement();
+			break;
 		}
 		
-		String reqBody = sb.toString();
-		return URLDecoder.decode(reqBody,HTTP.UTF_8);
+//		ServletInputStream inputStream = request.getInputStream();
+//		inputStream.reset();
+//		BufferedReader br =  new BufferedReader(new InputStreamReader(inputStream));
+//		String line = null;
+//		StringBuilder sb = new StringBuilder();
+//		while((line = br.readLine())!=null){
+//			sb.append(line);
+//		}
+//		
+//		String reqBody = sb.toString();
+//		return URLDecoder.decode(reqBody,HTTP.UTF_8);
+		return json;
 		
 	}
 
 
-	public   boolean reqGameServer(String jsonContent,ContentPojo contentPojo){
+	private boolean reqGameServer(String jsonContent,ContentPojo contentPojo){
 		
 		boolean success = false;
 		
@@ -77,12 +90,10 @@ public class ChargeService extends ActionSupport implements ServletRequestAware,
 			if(targetZone!=null){
 				Map<String,Object> params = new HashMap<String, Object>();
 				params.put("content", jsonContent);
-				System.out.println("ip= " + targetZone.getServerIp() + "  port=" + targetZone.getChargePort());
+				
 				String resp = HttpClientUtil.post(targetZone.getServerIp(),targetZone.getChargePort(), params);
 				success = StringUtils.contains(resp, "ok");
 				ChargeLog.info("charge", contentPojo.getCpTradeNo(), "游戏服处理结果："+resp);
-			}else{
-				System.out.println("targetZone == null ");
 			}
 			
 		} catch (Exception e) {			
