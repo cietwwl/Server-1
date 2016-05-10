bd6e5dfacf51a24ec72c4c6d4d168c33e84eb12f
allen <allen@rainwings.cn>
2016-05-10 16:10:06 +0800

    dev:补充测试用例

--- server/FSChargeWeb/src/main/java/com/fy/TestService.java
+++ server/FSChargeWeb/src/main/java/com/fy/TestService.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
 import com.fy.common.FastJsonUtil;
 import com.fy.http.HttpClientUtil;
@@ -12,11 +14,6 @@ import com.fy.http.HttpClientUtil;
 public class TestService {
 
 	public static void main(String[] args) throws Exception {
-		
-//		String test="%7B%22fee%22%3A0%2C%22gameId%22%3A0%2C%22itemAmount%22%3A0%2C%22money%22%3A0%2C%22serverId%22%3A1%7D";
-////		String utf = new String(test.getBytes("gb2312"),"UTF-8");
-//		String decode = URLDecoder.decode(test, "utf-8");
-//		System.out.println(decode);
 		
 		
 		ContentPojo content = new ContentPojo();
@@ -34,8 +31,13 @@ public class TestService {
 		HttpPost httppost = new HttpPost(url);
 		httppost.setEntity(new StringEntity(FastJsonUtil.toJson(content),"UTF-8"));
 		
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		httpClient.execute(httppost);
+		
 		String resp = HttpClientUtil.post("127.0.0.1", 9090, params);
 		System.out.println(resp);
 	}
+	
+	
 	
 }
