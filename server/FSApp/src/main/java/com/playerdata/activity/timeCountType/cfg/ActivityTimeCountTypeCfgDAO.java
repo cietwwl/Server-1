02cfd9afec03364c836ddec39f233f29b259b4e1
allen <allen@rainwings.cn>
2016-05-11 16:24:18 +0800

    DEV:增加对在线奖励的活动支持，没有功能，不影响现有代码

---
+++ server/FSApp/src/main/java/com/playerdata/activity/timeCountType/cfg/ActivityTimeCountTypeCfgDAO.java
@@ -0,0 +1,91 @@
+package com.playerdata.activity.timeCountType.cfg;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.playerdata.Player;
+import com.playerdata.activity.timeCountType.ActivityTimeCountTypeEnum;
+import com.playerdata.activity.timeCountType.ActivityTimeCountTypeHelper;
+import com.playerdata.activity.timeCountType.data.ActivityTimeCountTypeItem;
+import com.playerdata.activity.timeCountType.data.ActivityTimeCountTypeSubItem;
+import com.rw.fsutil.cacheDao.CfgCsvDao;
+import com.rw.fsutil.util.DateUtils;
+import com.rw.fsutil.util.SpringContextUtil;
+import com.rwbase.common.config.CfgCsvHelper;
+
+/*
+ * @author HC
+ * @date 2016年1月16日 下午5:42:44
+ * @Description 帮派的基础配置表Dao
+ */
+public final class ActivityTimeCountTypeCfgDAO extends CfgCsvDao<ActivityTimeCountTypeCfg> {
+
+
+	public static ActivityTimeCountTypeCfgDAO getInstance() {
+		return SpringContextUtil.getBean(ActivityTimeCountTypeCfgDAO.class);
+	}
+
+	
+	@Override
+	public Map<String, ActivityTimeCountTypeCfg> initJsonCfg() {
+		cfgCacheMap = CfgCsvHelper.readCsv2Map("Activity/ActivityCountTypeCfg.csv", ActivityTimeCountTypeCfg.class);
+		for (ActivityTimeCountTypeCfg cfgTmp : cfgCacheMap.values()) {
+			parseTime(cfgTmp);
+		}
+		
+		return cfgCacheMap;
+	}
+	
+	
+	public void parseTime(ActivityTimeCountTypeCfg cfgItem){
+		long startTime = DateUtils.YyyymmddhhmmToMillionseconds(cfgItem.getStartTimeStr());
+		cfgItem.setStartTime(startTime);
+		
+		long endTime = DateUtils.YyyymmddhhmmToMillionseconds(cfgItem.getEndTimeStr());
+		cfgItem.setEndTime(endTime);		
+	}
+		
+	
+	public ActivityTimeCountTypeCfg getConfig(String id){
+		ActivityTimeCountTypeCfg cfg = getCfgById(id);
+		return cfg;
+	}
+	
+	public ActivityTimeCountTypeItem newItem(Player player, ActivityTimeCountTypeEnum countTypeEnum){
+		
+		String cfgId = countTypeEnum.getCfgId();
+		ActivityTimeCountTypeCfg cfgById = getCfgById(cfgId );
+		if(cfgById!=null){			
+			ActivityTimeCountTypeItem item = new ActivityTimeCountTypeItem();
+			String itemId = ActivityTimeCountTypeHelper.getItemId(player.getUserId(), countTypeEnum);
+			item.setId(itemId);
+			item.setCfgId(cfgId);
+			item.setUserId(player.getUserId());
+			item.setVersion(cfgById.getVersion());
+			List<ActivityTimeCountTypeSubItem> newItemList = newItemList(player, cfgById);
+			item.setSubItemList(newItemList);
+			return item;
+		}else{
+			return null;
+		}		
+		
+	}
+	
+	
+	public List<ActivityTimeCountTypeSubItem> newItemList(Player player, ActivityTimeCountTypeCfg activityCountTypeCfg) {
+		List<ActivityTimeCountTypeSubItem> subItemList = new ArrayList<ActivityTimeCountTypeSubItem>();
+		List<ActivityTimeCountTypeSubCfg> subItemCfgList = ActivityTimeCountTypeSubCfgDAO.getInstance().getByParentCfgId(activityCountTypeCfg.getId());
+		for (ActivityTimeCountTypeSubCfg activityCountTypeSubCfg : subItemCfgList) {
+			ActivityTimeCountTypeSubItem subItem = new ActivityTimeCountTypeSubItem();
+			subItem.setCfgId(activityCountTypeSubCfg.getId());	
+			subItem.setCount(activityCountTypeSubCfg.getAwardCount());
+			subItemList.add(subItem);
+		}	
+		return subItemList;
+	}
+
+	
+
+
+}
