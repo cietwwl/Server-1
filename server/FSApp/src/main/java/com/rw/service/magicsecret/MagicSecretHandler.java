663612a0e1291e5050ef701d7eb791f50a5584f3
akenwang <akenwang@rainwings.cn>
2016-05-19 20:16:46 +0800

    添加了法宝秘境的service

---
+++ server/FSApp/src/main/java/com/rw/service/magicsecret/MagicSecretHandler.java
@@ -0,0 +1,78 @@
+package com.rw.service.magicsecret;
+
+import com.google.protobuf.ByteString;
+import com.playerdata.MagicSecretMgr;
+import com.playerdata.Player;
+import com.rwproto.MagicSecretProto.MagicSecretReqMsg;
+import com.rwproto.MagicSecretProto.MagicSecretRspMsg;
+import com.rwproto.MagicSecretProto.msResultType;
+
+public class MagicSecretHandler {
+
+	private static MagicSecretHandler instance;	
+
+	private MagicSecretHandler() {
+	}
+
+	public static MagicSecretHandler getInstance() {
+		if (instance == null) {
+			instance = new MagicSecretHandler();
+		}
+		return instance;
+	}
+
+	public ByteString getMSRankData(Player player, MagicSecretReqMsg msgMSRequest) {
+		String userId = player.getUserId();
+
+		MagicSecretRspMsg.Builder msRsp = MagicSecretRspMsg.newBuilder();
+		msRsp.setReqType(msgMSRequest.getReqType());
+
+		MagicSecretMgr msMgr = player.getMagicSecretMgr();
+		msRsp.setRstType(msResultType.SUCCESS);
+		return msRsp.build().toByteString();
+	}
+
+	public ByteString enterMSFight(Player player, MagicSecretReqMsg msgMSRequest) {
+		String userId = player.getUserId();
+
+		MagicSecretRspMsg.Builder msRsp = MagicSecretRspMsg.newBuilder();
+		msRsp.setReqType(msgMSRequest.getReqType());
+
+		MagicSecretMgr msMgr = player.getMagicSecretMgr();
+		msRsp.setRstType(msResultType.SUCCESS);
+		return msRsp.build().toByteString();
+	}
+
+	public ByteString getMSSingleReward(Player player, MagicSecretReqMsg msgMSRequest) {
+		String userId = player.getUserId();
+
+		MagicSecretRspMsg.Builder msRsp = MagicSecretRspMsg.newBuilder();
+		msRsp.setReqType(msgMSRequest.getReqType());
+
+		MagicSecretMgr msMgr = player.getMagicSecretMgr();
+		msRsp.setRstType(msResultType.SUCCESS);
+		return msRsp.build().toByteString();
+	}
+
+	public ByteString openRewardBox(Player player, MagicSecretReqMsg msgMSRequest) {
+		String userId = player.getUserId();
+
+		MagicSecretRspMsg.Builder msRsp = MagicSecretRspMsg.newBuilder();
+		msRsp.setReqType(msgMSRequest.getReqType());
+
+		MagicSecretMgr msMgr = player.getMagicSecretMgr();
+		msRsp.setRstType(msResultType.SUCCESS);
+		return msRsp.build().toByteString();
+	}
+
+	public ByteString exchangeBuff(Player player, MagicSecretReqMsg msgMSRequest) {
+		String userId = player.getUserId();
+
+		MagicSecretRspMsg.Builder msRsp = MagicSecretRspMsg.newBuilder();
+		msRsp.setReqType(msgMSRequest.getReqType());
+
+		MagicSecretMgr msMgr = player.getMagicSecretMgr();
+		msRsp.setRstType(msResultType.SUCCESS);
+		return msRsp.build().toByteString();
+	}
+}
---
+++ server/FSApp/src/main/java/com/rw/service/magicsecret/MagicSecretService.java
@@ -0,0 +1,48 @@
+package com.rw.service.magicsecret;
+
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.playerdata.Player;
+import com.rw.service.FsService;
+import com.rwproto.MagicSecretProto.MagicSecretReqMsg;
+import com.rwproto.MagicSecretProto.msRequestType;
+import com.rwproto.RequestProtos.Request;
+
+public class MagicSecretService implements FsService {
+
+	private MagicSecretHandler mHandler = MagicSecretHandler.getInstance();
+
+	@SuppressWarnings("finally")
+	public ByteString doTask(Request request, Player player) {
+		ByteString result = null;
+		try {
+			MagicSecretReqMsg msgMSRequest = MagicSecretReqMsg.parseFrom(request.getBody().getSerializedContent());
+			msRequestType msType = msgMSRequest.getReqType();
+			switch (msType) {
+			case GET_MS_RANK:
+				result = mHandler.getMSRankData(player, msgMSRequest);
+				break;
+			case ENTER_MS_FIGHT:
+				result = mHandler.enterMSFight(player, msgMSRequest);
+				break;
+			case GET_MS_SINGLE_REWARD:
+				result = mHandler.getMSSingleReward(player, msgMSRequest);
+				break;
+			case GET_MS_SWEEP_REWARD:
+				result = mHandler.getMSSingleReward(player, msgMSRequest);
+				break;
+			case EXCHANGE_BUFF:
+				result = mHandler.exchangeBuff(player, msgMSRequest);
+			case OPEN_REWARD_BOX:
+				result = mHandler.openRewardBox(player, msgMSRequest);
+				break;
+			default:
+				break;
+			}
+		} catch (InvalidProtocolBufferException e) {
+			e.printStackTrace();
+		} finally {
+			return result;
+		}
+	}
+}
