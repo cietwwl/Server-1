1150aff58372e48d59b91922db63315d22c960b5
HC <huchong@rainwings.cn>
2016-06-05 18:44:34 +0800

    dev：增加聚宝之地的金币

--- server/FSApp/src/main/java/com/rw/service/copy/TrailHandler.java
+++ server/FSApp/src/main/java/com/rw/service/copy/TrailHandler.java
@@ -8,6 +8,7 @@ import com.playerdata.Player;
 import com.playerdata.readonly.CopyLevelRecordIF;
 import com.rw.service.dailyActivity.Enum.DailyActivityType;
 import com.rw.service.pve.PveHandler;
+import com.rwbase.common.enu.eSpecialItemId;
 import com.rwbase.common.userEvent.UserEventMgr;
 import com.rwbase.dao.copy.cfg.CopyCfg;
 import com.rwbase.dao.copy.cfg.CopyCfgDAO;
@@ -29,8 +30,7 @@ public class TrailHandler {
 	/*
 	 * 试炼之境 战斗结算
 	 */
-	public ByteString battleClear(Player player, MsgCopyRequest copyRequest,int copyType)
-	{
+	public ByteString battleClear(Player player, MsgCopyRequest copyRequest, int copyType) {
 		MsgCopyResponse.Builder copyResponse = MsgCopyResponse.newBuilder();
 		int levelId = copyRequest.getTagBattleData().getLevelId();
 
@@ -47,36 +47,36 @@ public class TrailHandler {
 		// 铜钱 经验 体力 结算
 		PvECommonHelper.addPlayerAttr4Battle(player, copyCfg);
 
-		//TODO HC @Modify 2015-11-30 bug fix 没有把掉落物品放进去发送给玩家
+		// TODO HC @Modify 2015-11-30 bug fix 没有把掉落物品放进去发送给玩家
 		PvECommonHelper.addCopyRewards(player, copyCfg);
 
 		// 英雄经验
 		List<String> listUpHero = PvECommonHelper.addHerosExp(player, copyRequest, copyCfg);
 
 		player.getCopyDataMgr().subCopyCount(String.valueOf(levelId));
-		
 
 		TagBattleClearingResult.Builder tagBattleClearingResult = TagBattleClearingResult.newBuilder(); // 战斗结算返回的信息...
 		tagBattleClearingResult.addAllUpHeroId(listUpHero);// 升级英雄ID...
 		copyResponse.setTagBattleClearingResult(tagBattleClearingResult.build());
 		copyResponse.setLevelId(copyCfg.getLevelID());
 		copyResponse.setEResultType(EResultType.BATTLE_CLEAR);
-		
-		//练气山谷、聚宝之地日常任务
-		if(copyType==CopyType.COPY_TYPE_TRIAL_JBZD)
-		{
-			//聚宝之地
+
+		// 练气山谷、聚宝之地日常任务
+		if (copyType == CopyType.COPY_TYPE_TRIAL_JBZD) {
+			// 增加聚宝之地的金币
+			int addCoin = copyRequest.getTagBattleData().getFortuneResult().getDeductGoldCount();
+			if (addCoin > 0) {
+				player.getItemBagMgr().addItem(eSpecialItemId.Coin.getValue(), addCoin);
+			}
+			// 聚宝之地
 			player.getDailyActivityMgr().AddTaskTimesByType(DailyActivityType.Trial_JBZD, 1);
 			UserEventMgr.getInstance().TreasureLandCopyWinDaily(player, 1);
-		}
-		else
-		{
-		   //练气山谷
+		} else {
+			// 练气山谷
 			player.getDailyActivityMgr().AddTaskTimesByType(DailyActivityType.Trial_LQSG, 1);
 		}
-		
-		
-		//战斗结束，推送pve消息给前端
+
+		// 战斗结束，推送pve消息给前端
 		PveHandler.getInstance().sendPveInfo(player);
 		return copyResponse.build().toByteString();
 	}
@@ -84,7 +84,7 @@ public class TrailHandler {
 	/*
 	 * 扫荡关卡... 掉落------>[{"itemID":700108,"itemNum":1},{"itemID":803002,"itemNum":1}]
 	 */
-	public ByteString sweep(Player player, MsgCopyRequest copyRequest,int copyType) {
+	public ByteString sweep(Player player, MsgCopyRequest copyRequest, int copyType) {
 		MsgCopyResponse.Builder copyResponse = MsgCopyResponse.newBuilder();
 		int levelId = copyRequest.getLevelId();
 		CopyCfg copyCfg = CopyCfgDAO.getInstance().getCfg(levelId); // 地图的配置...
@@ -115,20 +115,17 @@ public class TrailHandler {
 		List<TagSweepInfo> listSweepInfo = PvECommonHelper.gainSweepRewards(player, times, copyCfg);
 
 		copyResponse.addAllTagSweepInfoList(listSweepInfo);
-		
-		//练气山谷、聚宝之地日常任务
-		if(copyType==CopyType.COPY_TYPE_TRIAL_JBZD)
-		{
-			//聚宝之地
+
+		// 练气山谷、聚宝之地日常任务
+		if (copyType == CopyType.COPY_TYPE_TRIAL_JBZD) {
+			// 聚宝之地
 			player.getDailyActivityMgr().AddTaskTimesByType(DailyActivityType.Trial_JBZD, 1);
-		} 
-		else 
-		{
-			//练气山谷
+		} else {
+			// 练气山谷
 			player.getDailyActivityMgr().AddTaskTimesByType(DailyActivityType.Trial_LQSG, 1);
 		}
-		
-		//战斗结束，推送pve消息给前端
+
+		// 战斗结束，推送pve消息给前端
 		PveHandler.getInstance().sendPveInfo(player);
 
 		return copyResponse.setEResultType(EResultType.SWEEP_SUCCESS).build().toByteString();
