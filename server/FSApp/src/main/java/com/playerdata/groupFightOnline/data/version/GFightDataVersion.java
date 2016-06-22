e495e9449100d40f42a7f9561b087c607cb9bc5f
allen <allen@rainwings.cn>
2016-06-22 15:47:18 +0800

    dev:添加帮派战版本控制基础类

---
+++ server/FSApp/src/main/java/com/playerdata/groupFightOnline/data/version/GFightDataVersion.java
@@ -0,0 +1,42 @@
+package com.playerdata.groupFightOnline.data.version;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GFightDataVersion {
+
+	private int biddingItem;
+	
+	private int defendArmyItem;
+	
+	private int onlineGroupData;
+	
+	private int onlineResourceData;
+	
+	private int userOnlineData;
+
+	public int getBiddingItem() {
+		return biddingItem;
+	}
+
+	public int getDefendArmyItem() {
+		return defendArmyItem;
+	}
+
+	public int getOnlineGroupData() {
+		return onlineGroupData;
+	}
+
+	public int getOnlineResourceData() {
+		return onlineResourceData;
+	}
+
+	public int getUserOnlineData() {
+		return userOnlineData;
+	}
+	
+
+	
+	
+	
+}
---
+++ server/FSApp/src/main/java/com/playerdata/groupFightOnline/data/version/GFightDataVersionMgr.java
@@ -0,0 +1,33 @@
+package com.playerdata.groupFightOnline.data.version;
+
+import com.bm.group.GroupBM;
+import com.playerdata.Player;
+import com.rw.fsutil.util.jackson.JsonUtil;
+import com.rwbase.dao.group.pojo.Group;
+
+public class GFightDataVersionMgr {
+
+	public static void synByVersion(Player player, String versionJson) {
+		// 基础数据版本号
+		String groupId = player.getUserGroupAttributeDataMgr().getUserGroupAttributeData().getGroupId();
+		Group group = GroupBM.get(groupId);
+		if (group == null) {
+			return;
+		}
+
+		GFightDataVersion groupDataVersion = fromJson(versionJson);
+		if (groupDataVersion == null) {
+			return;
+		}
+
+		
+	}
+	
+	
+
+	public static GFightDataVersion fromJson(String versionJson) {
+		GFightDataVersion groupDataVersion = JsonUtil.readValue(versionJson, GFightDataVersion.class);
+		return groupDataVersion;
+	}
+
+}
