dc56f3fd00042f47a7ecc7b8df53cc3784f6a19a
Jamaz <jamazruan@rainwings.cn>
2016-06-23 11:00:17 +0800

    bugfix:设置竞技场默认攻击阵容(使用templateId而非uuid)

--- server/FSApp/src/main/java/com/bm/arena/ArenaBM.java
+++ server/FSApp/src/main/java/com/bm/arena/ArenaBM.java
@@ -176,15 +176,18 @@ public class ArenaBM {
 
 		List<Hero> maxFightingHeros = player.getHeroMgr().getMaxFightingHeros();
 		ArrayList<String> defaultHeros = new ArrayList<String>(4);
+		ArrayList<String> defaultAtkHeros = new ArrayList<String>(4);
 		for (Hero hero : maxFightingHeros) {
 			String heroId = hero.getUUId();
 			if (!heroId.equals(userId)) {
 				defaultHeros.add(heroId);
+				defaultAtkHeros.add(hero.getTemplateId());
 			}
 		}
 
 		data.setHeroIdList(defaultHeros);
-		data.setAtkHeroList(new ArrayList<String>(defaultHeros));
+		data.setAtkHeroList(defaultAtkHeros);
+		
 		ArenaInfoCfg infoCfg = ArenaInfoCfgDAO.getInstance().getArenaInfo();
 		data.setRemainCount(infoCfg.getCount());
 		data.setHeadImage(headImage);
