8d3b6ff510678f895f26880040346c86c88c40d3
yxf <xiaofeiyang@rainwings.cn>
2016-06-24 17:34:16 +0800

    6-4 触发的事件

---
+++ server/FSApp/src/main/java/com/rwbase/common/userEvent/vitalityTypeEventHandler/UserEventStrengthenMagicVitalityHandler.java
@@ -0,0 +1,55 @@
+package com.rwbase.common.userEvent.vitalityTypeEventHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.log.GameLog;
+import com.log.LogModule;
+import com.playerdata.Player;
+import com.playerdata.activity.VitalityType.ActivityVitalityTypeEnum;
+import com.playerdata.activity.VitalityType.ActivityVitalityTypeMgr;
+import com.playerdata.activity.VitalityType.cfg.ActivityVitalityCfgDAO;
+import com.playerdata.activity.VitalityType.cfg.ActivityVitalitySubCfg;
+import com.playerdata.activity.VitalityType.cfg.ActivityVitalitySubCfgDAO;
+import com.rwbase.common.userEvent.IUserEventHandler;
+import com.rwbase.common.userEvent.eventHandler.UserEventHandleTask;
+
+public class UserEventStrengthenMagicVitalityHandler implements IUserEventHandler{
+	private List<UserEventHandleTask> eventTaskList = new ArrayList<UserEventHandleTask>();
+	public UserEventStrengthenMagicVitalityHandler(){
+		init();	
+	}
+	
+	private void init(){
+		eventTaskList.add(new UserEventHandleTask() {
+			@Override
+			public void doAction(Player player, Object params) {
+				ActivityVitalitySubCfg subCfg = ActivityVitalitySubCfgDAO.getInstance().getByType(ActivityVitalityTypeEnum.StrengthenMagicVitality.getCfgId());
+				
+				boolean isLevelEnough = ActivityVitalityTypeMgr.getInstance().isLevelEnough(player);
+				if(subCfg!=null&&isLevelEnough){
+					ActivityVitalityTypeMgr.getInstance().addCount(player, ActivityVitalityTypeEnum.StrengthenMagicVitality,subCfg, Integer.parseInt(params.toString()));
+					GameLog.error(LogModule.ComActivityVitality, "userId:"+player.getUserId(), "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~活动之王-送体开启",null);
+					}
+				}
+			@Override
+			public void logError(Player player,Throwable ex) {
+				StringBuilder reason = new StringBuilder(ActivityVitalityTypeEnum.StrengthenMagicVitality.toString()).append(" error");				
+				GameLog.error(LogModule.ComActivityVitality, "userId:"+player.getUserId(), reason.toString(),ex);
+			}						
+		});
+		
+	}
+	
+	@Override
+	public void doEvent(Player player, Object params) {
+		
+		for (UserEventHandleTask userEventHandleTask : eventTaskList) {
+			userEventHandleTask.doWrapAction(player, params);	
+		}
+		
+	}
+	
+	
+
+}
