bee2decf041058cd70267c3db14edcc25e94542c
HC <huchong@rainwings.cn>
2016-07-10 22:40:38 +0800

    dev：屏蔽技能打印

--- server/FSApp/src/main/java/com/playerdata/SkillMgr.java
+++ server/FSApp/src/main/java/com/playerdata/SkillMgr.java
@@ -7,7 +7,6 @@ import java.util.concurrent.TimeUnit;
 import org.apache.commons.lang.StringUtils;
 
 import com.common.Action;
-import com.log.GameLog;
 import com.playerdata.readonly.SkillMgrIF;
 import com.rw.service.skill.SkillConstant;
 import com.rwbase.common.enu.EPrivilegeDef;
@@ -313,7 +312,7 @@ public class SkillMgr extends IDataMgr implements SkillMgrIF {
 
 		SkillHelper.checkAllSkill(itemList);// 检查所有的技能
 
-		StringBuilder sb = new StringBuilder();
+		// StringBuilder sb = new StringBuilder();
 		for (int i = 0, size = itemList.size(); i < size; i++) {
 			Skill skill = itemList.get(i);
 			if (skill == null) {
@@ -322,11 +321,12 @@ public class SkillMgr extends IDataMgr implements SkillMgrIF {
 
 			skillItemHolder.updateItem(m_pPlayer, skill);
 
-			sb.append(String.format("技能Order[%s],技能Id[%s],等级[%s],伤害[%s],额外[%s],系数[%s],buff{%s},selfBuff{%s}\n", skill.getOrder(), skill.getSkillId(), skill.getLevel(), skill.getSkillDamage(),
-					skill.getExtraDamage(), skill.getSkillRate(), skill.getBuffId().toString(), skill.getSelfBuffId().toString()));
+			// sb.append(String.format("技能Order[%s],技能Id[%s],等级[%s],伤害[%s],额外[%s],系数[%s],buff{%s},selfBuff{%s}\n", skill.getOrder(),
+			// skill.getSkillId(), skill.getLevel(), skill.getSkillDamage(),
+			// skill.getExtraDamage(), skill.getSkillRate(), skill.getBuffId().toString(), skill.getSelfBuffId().toString()));
 		}
 
-		GameLog.info("升级技能模块", "升级后所有效果", sb.toString());
+		// GameLog.info("升级技能模块", "升级后所有效果", sb.toString());
 	}
 
 	/**
