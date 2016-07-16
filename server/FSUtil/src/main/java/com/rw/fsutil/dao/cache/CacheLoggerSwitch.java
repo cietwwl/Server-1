d5df9fe0680134937cef9cfc47cbb6899981013a
lida <lida@rainwings.cn>
2016-07-16 12:08:35 +0800

    dev:增加gm指令

--- server/FSUtil/src/main/java/com/rw/fsutil/dao/cache/CacheLoggerSwitch.java
+++ server/FSUtil/src/main/java/com/rw/fsutil/dao/cache/CacheLoggerSwitch.java
@@ -1,8 +1,17 @@
 package com.rw.fsutil.dao.cache;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 缓存日志开关管理类
+ * @author lida
+ *
+ */
 public class CacheLoggerSwitch {
 	
 	private boolean cacheLoggerSwitch = true;
+	private List<String> TrackList = new ArrayList<String>(); 
 	
 	public static CacheLoggerSwitch instance = new CacheLoggerSwitch();
 	
@@ -16,5 +25,25 @@ public class CacheLoggerSwitch {
 
 	public void setCacheLoggerSwitch(boolean cacheLoggerSwitch) {
 		this.cacheLoggerSwitch = cacheLoggerSwitch;
+	}
+
+	public List<String> getTrackList() {
+		return TrackList;
+	}
+
+	public void setTrackList(List<String> trackList) {
+		TrackList = trackList;
+	}
+	
+	public void clearTrackList(){
+		TrackList.clear();
+	}
+	
+	public void removeTrackList(String userId){
+		TrackList.remove(userId);
+	}
+	
+	public void addTrackList(String userId){
+		TrackList.add(userId);
 	}
 }
