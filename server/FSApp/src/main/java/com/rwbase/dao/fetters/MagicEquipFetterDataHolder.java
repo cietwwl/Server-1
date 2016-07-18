95feb8ca03dd91b861f98f85877d03bf74f9e07f
Alex <alexzhao@rainwings.cn>
2016-07-18 09:57:12 +0800

    添加法宝神器羁绊holder

---
+++ server/FSApp/src/main/java/com/rwbase/dao/fetters/MagicEquipFetterDataHolder.java
@@ -0,0 +1,16 @@
+package com.rwbase.dao.fetters;
+
+import com.rwproto.DataSynProtos.eSynType;
+
+
+/**
+ * 法宝神器羁绊holder
+ * @author Alex
+ *
+ * 2016年7月18日 上午9:54:58
+ */
+public class MagicEquipFetterDataHolder {
+
+	
+	private static final eSynType syType = eSynType.Magic
+}
