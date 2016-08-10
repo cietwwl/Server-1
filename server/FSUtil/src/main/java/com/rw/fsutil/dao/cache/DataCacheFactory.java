167f2ab5d925905dfcca0eb3908e7d4fa392b00a
Jamaz <jamazruan@rainwings.cn>
2016-08-10 11:08:00 +0800

    dev:临时修改支持创建不同类型的DataCache

--- server/FSUtil/src/main/java/com/rw/fsutil/dao/cache/DataCacheFactory.java
+++ server/FSUtil/src/main/java/com/rw/fsutil/dao/cache/DataCacheFactory.java
@@ -4,38 +4,50 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import com.rw.fsutil.common.Pair;
 import com.rw.fsutil.dao.common.DBThreadPoolMgr;
 
 @SuppressWarnings("rawtypes")
 public class DataCacheFactory {
 
-	private static ConcurrentHashMap<Class<?>, DataCache<?, ?>> cacheMap = new ConcurrentHashMap<Class<?>, DataCache<?, ?>>();
+	private static ConcurrentHashMap<Pair<Class, String>, DataCache<?, ?>> cacheMap = new ConcurrentHashMap<Pair<Class, String>, DataCache<?, ?>>();
 
-	public static <K, V> DataCache<K, V> createDataDache(Class<?> clazz, int initialCapacity, int maxCapacity, int updatePeriod, PersistentLoader<K, V> loader, DataNotExistHandler<K, V> handler) {
+	public static <K, V> DataCache<K, V> createDataDache(Class clazz, String name, int initialCapacity, int maxCapacity, int updatePeriod, PersistentLoader<K, V> loader,
+			DataNotExistHandler<K, V> handler) {
 		DataCache oldCache = cacheMap.get(clazz);
 		if (oldCache != null) {
-			System.err.println("DataCache名字重复1：" + clazz);
+			System.err.println("fatal error DataCache duplicate name1:" + clazz);
 			return oldCache;
 		}
-		DataCache<K, V> cache = new DataCache<K, V>(clazz.getSimpleName(), initialCapacity, maxCapacity, updatePeriod, DBThreadPoolMgr.getExecutor(), loader, handler);
-		oldCache = cacheMap.putIfAbsent(clazz, cache);
+		if (name == null || name.isEmpty()) {
+			throw new ExceptionInInitializerError("cache name is empty:" + name);
+		}
+		Pair<Class, String> key = Pair.Create(clazz, name);
+		DataCache<K, V> cache = new DataCache<K, V>(name, initialCapacity, maxCapacity, updatePeriod, DBThreadPoolMgr.getExecutor(), loader, handler);
+		oldCache = cacheMap.putIfAbsent(key, cache);
 		if (oldCache == null) {
 			return cache;
 		} else {
-			System.err.println("DataCache名字重复2：" + clazz);
+			System.err.println("fatal error DataCache duplicate name1:" + clazz);
 			return oldCache;
 		}
+	}
+
+	public static <K, V> DataCache<K, V> createDataDache(Class<?> clazz, int initialCapacity, int maxCapacity, int updatePeriod, PersistentLoader<K, V> loader, DataNotExistHandler<K, V> handler) {
+		return createDataDache(clazz, clazz.getSimpleName(), initialCapacity, maxCapacity, updatePeriod, loader, handler);
 	}
 
 	public static <K, V> DataCache<K, V> createDataDache(Class<?> clazz, int initialCapacity, int maxCapacity, int updatePeriod, PersistentLoader<K, V> loader) {
 		return createDataDache(clazz, initialCapacity, maxCapacity, updatePeriod, loader, null);
 	}
 
-	public static Map<Class<?>, Integer> getCacheStat() {
-		HashMap<Class<?>, Integer> map = new HashMap<Class<?>, Integer>();
-		for (Map.Entry<Class<?>, DataCache<?, ?>> entry : cacheMap.entrySet()) {
+	public static Map<String, Integer> getCacheStat() {
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		for (Map.Entry<Pair<Class, String>, DataCache<?, ?>> entry : cacheMap.entrySet()) {
 			DataCache cache = entry.getValue();
-			map.put(entry.getKey(), cache.getMaxCapacity() - cache.size());
+			// TODO 可能出现名字重复的情况
+			map.put(entry.getKey().getT2(), cache.getMaxCapacity() - cache.size());
 		}
 		return map;
 	}
