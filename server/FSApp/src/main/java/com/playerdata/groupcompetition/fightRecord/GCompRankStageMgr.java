ada4335ae31da29526cb8e4da0d189eb5c4f758c
akenwang <akenwang@rainwings.cn>
2016-08-24 16:01:30 +0800

    临时提交

---
+++ server/FSApp/src/main/java/com/playerdata/groupcompetition/fightRecord/GCompRankStageMgr.java
@@ -0,0 +1,19 @@
+package com.playerdata.groupcompetition.fightRecord;
+
+
+/**
+ * 争霸赛排行榜的阶段管理
+ * @author aken
+ */
+public class GCompRankStageMgr {
+	
+	private static GCompRankStageMgr instance = new GCompRankStageMgr();
+	
+	public static GCompRankStageMgr getInstance(){
+		return instance;
+	}
+	
+	public void stageEnd(String stageId){
+		
+	}
+}
