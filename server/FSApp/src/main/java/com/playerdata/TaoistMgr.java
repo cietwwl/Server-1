d39a6d4e6745eea4cbf510e6934b69c9b8347766
franky <franky@rainwings.cn>
2016-08-30 15:26:19 +0800

    bug fix ; 道术升级通知战力提升

--- server/FSApp/src/main/java/com/playerdata/TaoistMgr.java
+++ server/FSApp/src/main/java/com/playerdata/TaoistMgr.java
@@ -74,6 +74,9 @@ public class TaoistMgr extends RandomMgr implements PlayerEventListener, ITaoist
 		taoistMagicEff.hold(effects);
 		if (fire) {
 			taoistMagicEff.fire(effects);
+			String uid = record.getUserId();
+			Player player = PlayerMgr.getInstance().find(uid);
+			player.getUserTmpGameDataFlag().setSynFightingAll(true);
 		}
 	}
 
