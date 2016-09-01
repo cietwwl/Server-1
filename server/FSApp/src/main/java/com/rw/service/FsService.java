f559173724e2af49750c44066ef1d57e1d50e781
lida <lida@rainwings.cn>
2016-09-01 10:42:21 +0800

    dev:增加注释

--- server/FSApp/src/main/java/com/rw/service/FsService.java
+++ server/FSApp/src/main/java/com/rw/service/FsService.java
@@ -11,10 +11,27 @@ import com.rwproto.RequestProtos.Request;
 
 public interface FsService <Msg extends GeneratedMessage, Type extends ProtocolMessageEnum>{
 	
+	/**
+	 * 消息分发 逻辑处理
+	 * @param request
+	 * @param player
+	 * @return
+	 */
 	public ByteString doTask(Msg request, Player player);
 	
+	/**
+	 * 消息解析统一入口
+	 * @param request 返回消息请求
+	 * @return
+	 * @throws InvalidProtocolBufferException
+	 */
 	public Msg parseMsg(Request request) throws InvalidProtocolBufferException;
 	
+	/**
+	 * 返回消息处理类型
+	 * @param request
+	 * @return
+	 */
 	public Type getMsgType(Msg request);
 	
 }
