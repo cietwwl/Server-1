386c3b6ad9fae3820a7866d2aa905499405bfbd3
franky <franky@rainwings.cn>
2016-09-05 12:09:49 +0800

    dev :  屏蔽测试代码

--- server/FSApp/src/main/java/com/rw/service/common/FunctionOpenLogic.java
+++ server/FSApp/src/main/java/com/rw/service/common/FunctionOpenLogic.java
@@ -7,14 +7,11 @@ import org.apache.commons.lang3.StringUtils;
 import com.common.RefParam;
 import com.google.protobuf.ProtocolMessageEnum;
 import com.playerdata.Player;
-import com.playerdata.fixEquip.FixEquipHelper;
-import com.playerdata.fixEquip.FixEquipService;
 import com.rwbase.dao.openLevelLimit.CfgOpenLevelLimitDAO;
 import com.rwbase.dao.openLevelLimit.eOpenLevelType;
 import com.rwbase.dao.openLevelLimit.pojo.CfgOpenLevelLimit;
 import com.rwproto.FixEquipProto.RequestType;
 import com.rwproto.MsgDef.Command;
-import com.rwproto.RequestProtos;
 import com.rwproto.RequestProtos.Request;
 
 public class FunctionOpenLogic {
@@ -25,10 +22,11 @@ public class FunctionOpenLogic {
 		}
 		return instance;
 	}
-	public boolean passed = true;
+	
 	public boolean isOpen(ProtocolMessageEnum msgType, Request request,Player player){
-		passed = true;
-		if (passed) return true;
+		//调试用，请保留
+//		boolean passed = false;
+//		if (passed) return true;
 		CfgOpenLevelLimitDAO helper = CfgOpenLevelLimitDAO.getInstance();
 		Command cmd = request.getHeader().getCommand();
 		List<CfgOpenLevelLimit> cfgLst = helper.getOpenCfg(cmd);
