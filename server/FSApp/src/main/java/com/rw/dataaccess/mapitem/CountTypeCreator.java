0ddee5261fdfc61a66d933f65eaad190fed94c31
yxf <xiaofeiyang@rainwings.cn>
2016-09-10 10:11:07 +0800

    Revert "Revert "Merge branch 'release_temp82' of 192.168.2.254:server into release_temp82""
    This reverts commit ea62d444228d25d67fc6896501011bfaf2e7ae9b.
    
    Conflicts:
    	server/FSApp/src/main/java/com/playerdata/activity/exChangeType/ActivityExchangeTypeMgr.java
    	server/FSApp/src/main/java/com/playerdata/activity/fortuneCatType/cfg/ActivityFortuneCatTypeCfgDAO.java

--- server/FSApp/src/main/java/com/rw/dataaccess/mapitem/CountTypeCreator.java
+++ server/FSApp/src/main/java/com/rw/dataaccess/mapitem/CountTypeCreator.java
@@ -4,22 +4,20 @@ import java.util.List;
 
 import com.playerdata.activity.countType.ActivityCountTypeMgr;
 import com.playerdata.activity.countType.data.ActivityCountTypeItem;
+import com.playerdata.activity.countType.data.ActivityCountTypeItemHolder;
 
 public class CountTypeCreator implements MapItemCreator<ActivityCountTypeItem>{
 
 	@Override
 	public List<ActivityCountTypeItem> create(String userId,
 			MapItemValidateParam param) {
-		// TODO Auto-generated method stub
-		return null;
+		ActivityCountTypeItemHolder dataHolder = ActivityCountTypeItemHolder.getInstance();
+		return ActivityCountTypeMgr.getInstance().creatItems(userId, dataHolder.getItemStore(userId));
 	}
 
 	@Override
 	public boolean isOpen(MapItemValidateParam param) {
 		// TODO Auto-generated method stub
 		return ActivityCountTypeMgr.getInstance().isOpen(param);
-	}
-
-	
-	
+	}	
 }
