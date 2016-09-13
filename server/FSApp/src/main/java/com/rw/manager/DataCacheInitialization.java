5875f9032d23892740715e56f59b4bd2591452cf
Jamaz <jamazruan@rainwings.cn>
2016-09-13 21:05:37 +0800

    dev:调整DataCache支持Name & entityClass,修改DataChangedListenRegister方式，直接引用枚举

--- server/FSApp/src/main/java/com/rw/manager/DataCacheInitialization.java
+++ server/FSApp/src/main/java/com/rw/manager/DataCacheInitialization.java
@@ -5,31 +5,18 @@ import java.util.HashMap;
 import java.util.List;
 
 import com.common.HPCUtil;
-import com.playerdata.Player;
 import com.rw.fsutil.common.Pair;
+import com.rw.fsutil.dao.cache.CacheKey;
 import com.rw.fsutil.dao.cache.DataCacheFactory;
 import com.rw.fsutil.dao.cache.trace.DataChangedEvent;
 import com.rw.fsutil.dao.cache.trace.DataChangedVisitor;
 import com.rw.fsutil.dao.cache.trace.DataValueParser;
-import com.rw.trace.DataChangeListenRegister;
 import com.rw.trace.CreateTrace;
-import com.rwbase.dao.chat.pojo.UserPrivateChat;
-import com.rwbase.dao.gameNotice.TableGameNotice;
-import com.rwbase.dao.group.pojo.db.GroupLogData;
-import com.rwbase.dao.guide.pojo.UserGuideProgress;
-import com.rwbase.dao.guide.pojo.UserPlotProgress;
-import com.rwbase.dao.serverData.ServerData;
-import com.rwbase.dao.user.UserIdCache;
-import com.rwbase.dao.zone.TableZoneInfo;
+import com.rw.trace.DataChangeListenRegister;
 
 public class DataCacheInitialization {
 
 	public static void init() {
-		Class[] classArray = { ServerData.class, UserIdCache.class, TableZoneInfo.class, TableGameNotice.class, Player.class, GroupLogData.class, UserPrivateChat.class, UserPlotProgress.class, UserGuideProgress.class, GroupLogData.class };
-		ArrayList<String> ignoreList = new ArrayList<String>(classArray.length);
-		for (int i = 0; i < classArray.length; i++) {
-			ignoreList.add(classArray[i].getName());
-		}
 		HashMap<Class<?>, DataValueParser<?>> map = new HashMap<Class<?>, DataValueParser<?>>();
 		try {
 			List<Class<? extends DataValueParser>> list = HPCUtil.getAllAssignedClass(DataValueParser.class, CreateTrace.PARSER_PATH);
@@ -41,14 +28,15 @@ public class DataCacheInitialization {
 			e.printStackTrace();
 		}
 		DataChangeListenRegister[] changeArray = DataChangeListenRegister.values();
-		List<Pair<Class<?>, Class<? extends DataChangedVisitor<DataChangedEvent<?>>>>> dataChangeListeners = new ArrayList<Pair<Class<?>, Class<? extends DataChangedVisitor<DataChangedEvent<?>>>>>();
+		List<Pair<CacheKey, Class<? extends DataChangedVisitor<DataChangedEvent<?>>>>> dataChangeListeners = new ArrayList<Pair<CacheKey, Class<? extends DataChangedVisitor<DataChangedEvent<?>>>>>();
 		for (DataChangeListenRegister listener : changeArray) {
-			Class<?> traceClass = listener.getTraceClass();
+			CacheKey key = listener.getTraceClass().getDataCacheKey();
 			Class<? extends DataChangedVisitor<?>> listenerClass = listener.getListenerClass();
-			Pair<Class<?>, Class<? extends DataChangedVisitor<DataChangedEvent<?>>>> pair = Pair.<Class<?>, Class<? extends DataChangedVisitor<DataChangedEvent<?>>>> Create(traceClass, (Class<? extends DataChangedVisitor<DataChangedEvent<?>>>) listenerClass);
+			Pair<CacheKey, Class<? extends DataChangedVisitor<DataChangedEvent<?>>>> pair = 
+					Pair.<CacheKey, Class<? extends DataChangedVisitor<DataChangedEvent<?>>>> Create(key, (Class<? extends DataChangedVisitor<DataChangedEvent<?>>>) listenerClass);
 			dataChangeListeners.add(pair);
 		}
-		DataCacheFactory.init(ignoreList, map, dataChangeListeners);
+		DataCacheFactory.init(map, dataChangeListeners);
 	}
 
 }
---
+++ server/FSApp/src/main/java/com/rw/trace/support/UserExtendInfoParser.java
@@ -0,0 +1,90 @@
+package com.rw.trace.support;
+
+import com.rw.fsutil.dao.cache.record.JsonValueWriter;
+import com.rwbase.dao.user.UserExtendInfo;
+import com.rw.fsutil.dao.cache.trace.DataValueParser;
+import com.alibaba.fastjson.JSONObject;
+
+public class UserExtendInfoParser implements DataValueParser<UserExtendInfo> {
+
+    private JsonValueWriter writer = JsonValueWriter.getInstance();
+
+    @Override
+    public UserExtendInfo copy(UserExtendInfo entity) {
+        UserExtendInfo userExtendInfoCopy = new UserExtendInfo();
+        userExtendInfoCopy.setBlockReason(entity.getBlockReason());
+        userExtendInfoCopy.setBlockCoolTime(entity.getBlockCoolTime());
+        userExtendInfoCopy.setChatBanReason(entity.getChatBanReason());
+        userExtendInfoCopy.setChatBanCoolTime(entity.getChatBanCoolTime());
+        userExtendInfoCopy.setFeedbackId(entity.getFeedbackId());
+        return userExtendInfoCopy;
+    }
+
+    @Override
+    public JSONObject recordAndUpdate(UserExtendInfo entity1, UserExtendInfo entity2) {
+        JSONObject jsonMap = null;
+        String blockReason1 = entity1.getBlockReason();
+        String blockReason2 = entity2.getBlockReason();
+        if (!writer.equals(blockReason1, blockReason2)) {
+            entity1.setBlockReason(blockReason2);
+            jsonMap = writer.write(jsonMap, "blockReason", blockReason2);
+        }
+        long blockCoolTime1 = entity1.getBlockCoolTime();
+        long blockCoolTime2 = entity2.getBlockCoolTime();
+        if (blockCoolTime1 != blockCoolTime2) {
+            entity1.setBlockCoolTime(blockCoolTime2);
+            jsonMap = writer.write(jsonMap, "blockCoolTime", blockCoolTime2);
+        }
+        String chatBanReason1 = entity1.getChatBanReason();
+        String chatBanReason2 = entity2.getChatBanReason();
+        if (!writer.equals(chatBanReason1, chatBanReason2)) {
+            entity1.setChatBanReason(chatBanReason2);
+            jsonMap = writer.write(jsonMap, "chatBanReason", chatBanReason2);
+        }
+        long chatBanCoolTime1 = entity1.getChatBanCoolTime();
+        long chatBanCoolTime2 = entity2.getChatBanCoolTime();
+        if (chatBanCoolTime1 != chatBanCoolTime2) {
+            entity1.setChatBanCoolTime(chatBanCoolTime2);
+            jsonMap = writer.write(jsonMap, "chatBanCoolTime", chatBanCoolTime2);
+        }
+        int feedbackId1 = entity1.getFeedbackId();
+        int feedbackId2 = entity2.getFeedbackId();
+        if (feedbackId1 != feedbackId2) {
+            entity1.setFeedbackId(feedbackId2);
+            jsonMap = writer.write(jsonMap, "feedbackId", feedbackId2);
+        }
+        return jsonMap;
+    }
+
+    @Override
+    public boolean hasChanged(UserExtendInfo entity1, UserExtendInfo entity2) {
+        if (!writer.equals(entity1.getBlockReason(), entity2.getBlockReason())) {
+            return true;
+        }
+        if (entity1.getBlockCoolTime() != entity2.getBlockCoolTime()) {
+            return true;
+        }
+        if (!writer.equals(entity1.getChatBanReason(), entity2.getChatBanReason())) {
+            return true;
+        }
+        if (entity1.getChatBanCoolTime() != entity2.getChatBanCoolTime()) {
+            return true;
+        }
+        if (entity1.getFeedbackId() != entity2.getFeedbackId()) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public JSONObject toJson(UserExtendInfo entity) {
+        JSONObject json = new JSONObject(5);
+        json.put("blockReason", entity.getBlockReason());
+        json.put("blockCoolTime", entity.getBlockCoolTime());
+        json.put("chatBanReason", entity.getChatBanReason());
+        json.put("chatBanCoolTime", entity.getChatBanCoolTime());
+        json.put("feedbackId", entity.getFeedbackId());
+        return json;
+    }
+
+}
