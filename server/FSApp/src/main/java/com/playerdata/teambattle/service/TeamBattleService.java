76f3bc8c75364f8cc8d8c4f2ea690ad16a9f678a
akenwang <akenwang@rainwings.cn>
2016-09-18 23:13:03 +0800

    漏写break语句的修复

--- server/FSApp/src/main/java/com/playerdata/teambattle/service/TeamBattleService.java
+++ server/FSApp/src/main/java/com/playerdata/teambattle/service/TeamBattleService.java
@@ -73,8 +73,10 @@ public class TeamBattleService implements FsService<TeamBattleReqMsg, TBRequestT
 				break;
 			case BUY_TIMES:
 				result = mHandler.buyBattleTimes(player, request);
+				break;
 			case ADD_ROBOT:
 				result = mHandler.addRobot(player, request);
+				break;
 			default:
 				GameLog.error(LogModule.TeamBattle, player.getUserId(), "接收到了一个Unknown的消息，无法处理", null);
 				break;
