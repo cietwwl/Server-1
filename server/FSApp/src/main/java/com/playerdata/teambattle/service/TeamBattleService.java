4ffa25af9a93800aca440dc85f9f2ab27aa76df2
allen <allen@rainwings.cn>
2016-09-22 19:50:03 +0800

    dev: 心魔录ai开发

--- server/FSApp/src/main/java/com/playerdata/teambattle/service/TeamBattleService.java
+++ server/FSApp/src/main/java/com/playerdata/teambattle/service/TeamBattleService.java
@@ -73,6 +73,8 @@ public class TeamBattleService implements FsService<TeamBattleReqMsg, TBRequestT
 				break;
 			case BUY_TIMES:
 				result = mHandler.buyBattleTimes(player, request);
+			case ADD_ROBOT:
+				result = mHandler.buyBattleTimes(player, request);
 			default:
 				GameLog.error(LogModule.TeamBattle, player.getUserId(), "接收到了一个Unknown的消息，无法处理", null);
 				break;
