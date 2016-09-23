2e1f5a3be34b0224611de382ee59c185353cb0df
perrychen <perrychen@rainwings.cn>
2016-09-23 12:10:59 +0800

    角色上线同步战力称号

--- server/FSApp/src/main/java/com/playerdata/fightinggrowth/FSUserFightingGrowthData.java
+++ server/FSApp/src/main/java/com/playerdata/fightinggrowth/FSUserFightingGrowthData.java
@@ -4,15 +4,21 @@ import javax.persistence.Id;
 
 import org.codehaus.jackson.annotate.JsonAutoDetect;
 import org.codehaus.jackson.annotate.JsonAutoDetect.Visibility;
+
+import com.playerdata.dataSyn.annotation.IgnoreSynField;
+import com.playerdata.dataSyn.annotation.SynClass;
+
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.annotate.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonAutoDetect(getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE)
+@SynClass
 public class FSUserFightingGrowthData {
 
 	@JsonProperty("1")
 	@Id
+	@IgnoreSynField
 	private String userId; // 所属的玩家的UserId
 	@JsonProperty("2")
 	private String _currentTitleKey; // 当前的称号等级
