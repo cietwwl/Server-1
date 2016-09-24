56fe0879bd74b7f18129be9c4624c3fb0c901542
akenwang <akenwang@rainwings.cn>
2016-09-24 08:31:25 +0800

    争霸赛备战区内的走动测试

--- server/RobotPressureTest/src/main/java/com/rw/handler/groupCompetition/service/GroupCompSameSceneHandler.java
+++ server/RobotPressureTest/src/main/java/com/rw/handler/groupCompetition/service/GroupCompSameSceneHandler.java
@@ -7,112 +7,63 @@ import com.google.protobuf.InvalidProtocolBufferException;
 import com.rw.Client;
 import com.rw.common.MsgReciver;
 import com.rw.common.RobotLog;
-import com.rw.handler.groupCompetition.data.guess.GCQuizEventItem;
-import com.rw.handler.groupCompetition.data.guess.GCQuizEventItemHolder;
 import com.rwproto.GrouFightOnlineProto.GFResultType;
+import com.rwproto.GroupCompetitionProto.AreaPosition;
+import com.rwproto.GroupCompetitionProto.CommonReqMsg;
+import com.rwproto.GroupCompetitionProto.CommonRspMsg;
 import com.rwproto.GroupCompetitionProto.GCRequestType;
 import com.rwproto.GroupCompetitionProto.GCResultType;
-import com.rwproto.GroupCompetitionProto.ReqAllGuessInfo;
-import com.rwproto.GroupCompetitionProto.ReqNewGuess;
-import com.rwproto.GroupCompetitionProto.RspAllGuessInfo;
-import com.rwproto.GroupCompetitionProto.RsqNewGuess;
 import com.rwproto.MsgDef.Command;
 import com.rwproto.ResponseProtos.Response;
 
 public class GroupCompSameSceneHandler {
 
-	public static final int[] quizCountArr = {100000, 200000, 500000};
-	
+	public static final float pxMin = -0.7f;
+	public static final float pxMax = 11.5f;
+	public static final float pyMin = -6f;
+	public static final float pyMax = -1f;
+
 	private static GroupCompSameSceneHandler handler = new GroupCompSameSceneHandler();
 
 	public static GroupCompSameSceneHandler getHandler() {
 		return handler;
 	}
 	
-	public boolean groupCompQuiz(Client client){
-		boolean result = getCanQuizMatch(client);
-		if (!result) {
-			RobotLog.fail("groupCompQuiz[send]争霸赛获取可竞猜的项目失败=" + result);
-			return result;
-		}
-		return quizForCompetion(client);
-	}
-	
 	/**
-	 * 获取当前的可竞猜项目
+	 * 备战区内走动
 	 * @param player
 	 * @param gcRsp
 	 */
-	private boolean getCanQuizMatch(Client client) {
-		ReqAllGuessInfo.Builder req = ReqAllGuessInfo.newBuilder();
-		req.setReqType(GCRequestType.GetCanGuessMatch);
-		boolean success = client.getMsgHandler().sendMsg(Command.MSG_GROUP_COMPETITION_QUIZ_SYN, req.build().toByteString(), new MsgReciver() {
-			@Override
-			public Command getCmd() {
-				return Command.MSG_GROUP_COMPETITION_QUIZ_SYN;
-			}
-
-			@Override
-			public boolean execute(Client client, Response response) {
-				ByteString serializedContent = response.getSerializedContent();
-				try {
-					RspAllGuessInfo rsp = RspAllGuessInfo.parseFrom(serializedContent);
-					if (rsp == null) {
-						RobotLog.fail("GroupCompetitionHandler[send] getCanQuizMatch转换响应消息为null");
-						return false;
-					}
-					GCResultType result = rsp.getRstType();
-					if (!result.equals(GFResultType.SUCCESS)) {
-						RobotLog.info("GroupCompetitionHandler[send] getCanQuizMatch服务器返回不成功 ");
-						return true;
-					}
-				} catch (InvalidProtocolBufferException e) {
-					RobotLog.fail("GroupCompetitionHandler[send] getCanQuizMatch失败", e);
-					return false;
-				}
-				return true;
-			}
-		});
-		return success;
-	}
-	
-	private boolean quizForCompetion(Client client){
-		GCQuizEventItem canQuizItem = GCQuizEventItemHolder.getInstance().getCanQuizItem();
-		if(null == canQuizItem){
-			return true;
-		}
+	public boolean informPreparePosition(Client client) {
 		Random rd = new Random();
-		String quizGroupId = canQuizItem.getGroupA().getGroupId();
-		if(1 == rd.nextInt(2)){
-			quizGroupId = canQuizItem.getGroupB().getGroupId();
-		}
-		ReqNewGuess.Builder req = ReqNewGuess.newBuilder();
-		req.setReqType(GCRequestType.NewGuess);
-		req.setCoin(quizCountArr[rd.nextInt(quizCountArr.length)]);
-		req.setGroupId(quizGroupId);
-		req.setMatchId(canQuizItem.getMatchId());
-		boolean success = client.getMsgHandler().sendMsg(Command.MSG_GROUP_COMPETITION_QUIZ, req.build().toByteString(), new MsgReciver() {
+		AreaPosition.Builder areaBuilder = AreaPosition.newBuilder();
+		areaBuilder.setX((pxMax - pxMin) * rd.nextFloat() + pxMin);
+		areaBuilder.setY((pyMax - pyMin) * rd.nextFloat() + pyMin);
+		CommonReqMsg.Builder req = CommonReqMsg.newBuilder();
+		req.setReqType(GCRequestType.InformPreparePosition);
+		req.setPosition(areaBuilder.build());
+		boolean success = client.getMsgHandler().sendMsg(Command.MSG_GROUP_COMPETITION, req.build().toByteString(), new MsgReciver() {
 			@Override
 			public Command getCmd() {
-				return Command.MSG_GROUP_COMPETITION_QUIZ;
+				return Command.MSG_GROUP_COMPETITION;
 			}
 
 			@Override
 			public boolean execute(Client client, Response response) {
 				ByteString serializedContent = response.getSerializedContent();
 				try {
-					RsqNewGuess rsp = RsqNewGuess.parseFrom(serializedContent);
+					CommonRspMsg rsp = CommonRspMsg.parseFrom(serializedContent);
 					if (rsp == null) {
-						RobotLog.fail("GroupCompetitionHandler[send] quizForCompetion转换响应消息为null");
+						RobotLog.fail("GroupCompSameSceneHandler[send] informPreparePosition转换响应消息为null");
 						return false;
 					}
 					GCResultType result = rsp.getRstType();
 					if (!result.equals(GFResultType.SUCCESS)) {
-						RobotLog.info("GroupCompetitionHandler[send] quizForCompetion服务器返回不成功 ");
+						RobotLog.info("GroupCompSameSceneHandler[send] informPreparePosition服务器返回不成功:" + rsp.getTipMsg());
 						return true;
 					}
 				} catch (InvalidProtocolBufferException e) {
-					RobotLog.fail("GroupCompetitionHandler[send] quizForCompetion失败", e);
+					RobotLog.fail("GroupCompSameSceneHandler[send] informPreparePosition失败", e);
 					return false;
 				}
 				return true;
