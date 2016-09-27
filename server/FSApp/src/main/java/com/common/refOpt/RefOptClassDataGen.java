ceab445f5aaf365184958830bf59f5050ca9885a
allen <allen@rainwings.cn>
2016-09-27 18:21:31 +0800

    dev:修正反射优化的bug

--- server/FSApp/src/main/java/com/common/refOpt/RefOptClassDataGen.java
+++ server/FSApp/src/main/java/com/common/refOpt/RefOptClassDataGen.java
@@ -1,6 +1,7 @@
 package com.common.refOpt;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,38 +14,38 @@ public class RefOptClassDataGen {
 		for (String className : classNameList) {
 			Class<?> targetClass = this.getClass().getClassLoader().loadClass(className);
 			
+			List<String> nameList = new ArrayList<String>();
+			Field[] sourceFields = targetClass.getDeclaredFields();
+			for (Field field : sourceFields) {
+				nameList.add(field.getName());
+			}
 			
-			String refGetBody = "{"+getRefInitCode(targetClass)+ getrefGetCode(targetClass)+"}";
+			String refGetBody = "{"+getRefInitCode(nameList)+ getrefGetCode(nameList)+"}";
 			dataMap.put(className, new RefOptClassData(className, refGetBody));
 		}
 		return dataMap;
 	}
 	
 	
-	private String getRefInitCode(Class<?> source) {
+	private String getRefInitCode(List<String> nameList) {
 		StringBuilder initMethod = new StringBuilder("if(!com.common.refOpt.RefNameIntMapper.hasClass(this)){ java.util.HashMap ref$FNmapTmp = new java.util.HashMap();");
 		
 		int fnIndex = 0;
-		Field[] sourceFields = source.getDeclaredFields();
-		for (Field field : sourceFields) {
-			String nameTmp = field.getName();
+		for (String nameTmp : nameList) {
 			initMethod.append("ref$FNmapTmp.put(\""+nameTmp+"\", new Integer("+fnIndex+"));");
+			fnIndex++;
 		}
 		initMethod.append("com.common.refOpt.RefNameIntMapper.put(this, ref$FNmapTmp);}");
 		
 		return initMethod.toString();
 	}
 	
-	private String getrefGetCode(Class<?> source) {
+	private String getrefGetCode(List<String> nameList) {
 		StringBuilder getMethod = new StringBuilder("int index = com.common.refOpt.RefNameIntMapper.get(this, $1);switch (index) {");
 		
 		int fnIndex = 0;
-		Field[] sourceFields = source.getDeclaredFields();
-		for (Field field : sourceFields) {
-			String nameTmp = field.getName();
-			if(!"ref$FNmap".equals(nameTmp)){
-				getMethod.append("case "+fnIndex+":return com.common.refOpt.Raw2ObjHelper.from(this."+nameTmp+");");
-			}
+		for (String nameTmp : nameList) {
+			getMethod.append("case "+fnIndex+":return com.common.refOpt.Raw2ObjHelper.from(this."+nameTmp+");");
 			fnIndex++;
 		}
 		getMethod.append("}return null;");
