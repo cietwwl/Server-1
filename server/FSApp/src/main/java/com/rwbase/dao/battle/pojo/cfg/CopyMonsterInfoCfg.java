8e24a5397de032bd80f1fdc9fbe88726d2699f16
Alex <alexzhao@rainwings.cn>
2016-09-29 20:36:29 +0800

    1.修复随机boss进入战斗血量不正确问题 2.armyInfo添加怪物位置逻辑

--- server/FSApp/src/main/java/com/rwbase/dao/battle/pojo/cfg/CopyMonsterInfoCfg.java
+++ server/FSApp/src/main/java/com/rwbase/dao/battle/pojo/cfg/CopyMonsterInfoCfg.java
@@ -1,9 +1,13 @@
 package com.rwbase.dao.battle.pojo.cfg;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.util.StringUtils;
+
+import com.playerdata.army.ArmyVector3;
 
 public class CopyMonsterInfoCfg {
 
@@ -19,19 +23,26 @@ public class CopyMonsterInfoCfg {
 	/**战斗时间 s*/
 	private int time;
 	
-	/**怪物1*/
-	private String Enemy_1;
-	
-	/**怪物2*/
-	private String Enemy_2;
-	
-	private String Enemy_3;
-	
-	private String Enemy_4;
-	
-	private String Enemy_5;
-	
-	private String Enemy_6;
+	private String Enemy_1; // 怪物1...
+	private int Enemy_1_x; // x轴位置...
+	private int Enemy_1_z; // z轴位置...
+	private String Enemy_2; // 怪物2...
+	private int Enemy_2_x; // x轴位置...
+	private int Enemy_2_z; // z轴位置...
+	private String Enemy_3; // 怪物3...
+	private int Enemy_3_x; // x轴位置...
+	private int Enemy_3_z; // z轴位置...
+	private String Enemy_4; // 怪物4...
+	private int Enemy_4_x; // x轴位置...
+	private int Enemy_4_z; // z轴位置...
+	private String Enemy_5; // 怪物5...
+	private int Enemy_5_x; // x轴位置...
+	private int Enemy_5_z; // z轴位置...
+	private String Enemy_6; // 怪物6...
+	private int Enemy_6_x; // x轴位置...
+	private int Enemy_6_z; // z轴位置...
+
+	private Map<String, ArmyVector3> positionMap = new HashMap<String, ArmyVector3>();
 	
 	private List<String> EnemyList = new ArrayList<String>();
 	
@@ -86,22 +97,33 @@ public class CopyMonsterInfoCfg {
 	public void formatData(){
 		if(!StringUtils.isEmpty(Enemy_1)){
 			EnemyList.add(Enemy_1);
+			positionMap.put(Enemy_1, new ArmyVector3(Enemy_1_x, 0, Enemy_1_z));
 		}
 		if(!StringUtils.isEmpty(Enemy_2)){
 			EnemyList.add(Enemy_2);
+			positionMap.put(Enemy_2, new ArmyVector3(Enemy_2_x, 0, Enemy_2_z));
 		}
 		if(!StringUtils.isEmpty(Enemy_3)){
 			EnemyList.add(Enemy_3);
+			positionMap.put(Enemy_3, new ArmyVector3(Enemy_3_x, 0, Enemy_3_z));
 		}
 		if(!StringUtils.isEmpty(Enemy_4)){
 			EnemyList.add(Enemy_4);
+			positionMap.put(Enemy_4, new ArmyVector3(Enemy_4_x, 0, Enemy_4_z));
 		}
 		if(!StringUtils.isEmpty(Enemy_5)){
 			EnemyList.add(Enemy_5);
+			positionMap.put(Enemy_5, new ArmyVector3(Enemy_5_x, 0, Enemy_5_z));
 		}
 		if(!StringUtils.isEmpty(Enemy_6)){
 			EnemyList.add(Enemy_6);
+			positionMap.put(Enemy_6, new ArmyVector3(Enemy_6_x, 0, Enemy_6_z));
 		}
 	}
 	
+	
+	
+	public ArmyVector3 getPosition(String enemyID){
+		return positionMap.get(enemyID);
+	}
 }
