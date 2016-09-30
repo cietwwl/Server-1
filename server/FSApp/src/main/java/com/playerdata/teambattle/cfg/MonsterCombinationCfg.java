c5d751c7130e9c34903d29884ee111283871457d
akenwang <akenwang@rainwings.cn>
2016-10-01 06:40:46 +0800

    组队的配置表更改

--- server/FSApp/src/main/java/com/playerdata/teambattle/cfg/MonsterCombinationCfg.java
+++ server/FSApp/src/main/java/com/playerdata/teambattle/cfg/MonsterCombinationCfg.java
@@ -4,9 +4,7 @@ import com.common.BaseConfig;
 public class MonsterCombinationCfg extends BaseConfig {
 	private String id; //id
 	private int battleTime; //战斗场次
-	private String monsterTeam1; //怪物队伍1
-	private String monsterTeam2; //怪物队伍2
-	private String monsterTeam3; //怪物队伍3
+	private int copyId; //copyId
 	private int scoreGain; //获胜可获得积分
 	private String mailReward; //邮件奖励
 	private int mail; //邮件id
@@ -18,20 +16,12 @@ public class MonsterCombinationCfg extends BaseConfig {
  	public int getBattleTime() {
  		return battleTime;
  	}
- 	
- 	public String getMonsterTeam1() {
- 		return monsterTeam1;
- 	}
- 	
- 	public String getMonsterTeam2() {
- 		return monsterTeam2;
- 	}
- 	
- 	public String getMonsterTeam3() {
- 		return monsterTeam3;
- 	}
- 	
- 	public int getScoreGain() {
+
+ 	public int getCopyId() {
+		return copyId;
+	}
+
+	public int getScoreGain() {
  		return scoreGain;
  	}
  	
