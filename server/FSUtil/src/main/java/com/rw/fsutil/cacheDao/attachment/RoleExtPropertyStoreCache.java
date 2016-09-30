7131e851f52d1b73657637aa07c4ddea87505c00
Jamaz <jamazruan@rainwings.cn>
2016-09-30 11:21:16 +0800

    bugfix:修复RoleExtProperty同步id使用错误的问题

--- server/FSUtil/src/main/java/com/rw/fsutil/cacheDao/attachment/RoleExtPropertyStoreCache.java
+++ server/FSUtil/src/main/java/com/rw/fsutil/cacheDao/attachment/RoleExtPropertyStoreCache.java
@@ -133,14 +133,14 @@ public class RoleExtPropertyStoreCache<T extends RoleExtProperty> implements Map
 		@Override
 		public boolean extractParams(CacheCompositKey<String, Integer> key, PlayerExtPropertyStoreImpl<T> value, List<Object[]> updateList) {
 			Integer key2 = key.getSecondKey();
-			T item = value.get(key2);
-			if (item == null) {
+			PlayerExtPropertyData<T> data = value.getItem(key2);
+			if (data == null) {
 				return false;
 			}
 			value.removeUpdateFlag(key2);
 			String ext;
 			try {
-				ext = mapper.writeValueAsString(item);
+				ext = mapper.writeValueAsString(data.getAttachment());
 			} catch (Exception e) {
 				// TODO Logger object info
 				e.printStackTrace();
@@ -149,7 +149,7 @@ public class RoleExtPropertyStoreCache<T extends RoleExtProperty> implements Map
 			if (ext == null) {
 				return false;
 			}
-			return updateList.add(new Object[] { ext, key });
+			return updateList.add(new Object[] { ext, data.getPrimaryKey() });
 		}
 
 		@Override
@@ -157,6 +157,7 @@ public class RoleExtPropertyStoreCache<T extends RoleExtProperty> implements Map
 			HashMap<Integer, PlayerExtPropertyData<T>> dirtyMap = value.getDirtyItems();
 			for (Map.Entry<Integer, PlayerExtPropertyData<T>> entry : dirtyMap.entrySet()) {
 				Integer k = entry.getKey();
+				PlayerExtPropertyData<T> data = entry.getValue();
 				String ext;
 				try {
 					ext = mapper.writeValueAsString(entry.getValue().getAttachment());
@@ -169,7 +170,7 @@ public class RoleExtPropertyStoreCache<T extends RoleExtProperty> implements Map
 					FSUtilLogger.error("extract params is null:" + key + "," + k + "," + cache.getName());
 					continue;
 				}
-				map.put(new DoubleKey<String, Integer>(key, k), new Object[] { ext, key });
+				map.put(new DoubleKey<String, Integer>(key, k), new Object[] { ext, data.getPrimaryKey() });
 			}
 			return true;
 		}
