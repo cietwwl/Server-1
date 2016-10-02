c2d03094deef3861e6746ff01a1edd86e7e67601
yxf <xiaofeiyang@rainwings.cn>
2016-10-02 16:04:11 +0800

    5-10 活动类型判断

--- server/FSApp/src/main/java/com/playerdata/activity/rateType/ActivityRateTypeMgr.java
+++ server/FSApp/src/main/java/com/playerdata/activity/rateType/ActivityRateTypeMgr.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 
+import com.log.GameLog;
 import com.playerdata.Player;
 import com.playerdata.activity.rateType.cfg.ActivityRateTypeCfg;
 import com.playerdata.activity.rateType.cfg.ActivityRateTypeCfgDAO;
@@ -24,19 +25,21 @@ public class ActivityRateTypeMgr {
 	}
 	
 	public boolean isActivityOnGoing(Player player, ActivityRateTypeEnum activityRateTypeEnum){
-		
 		ActivityRateTypeItemHolder dataHolder = ActivityRateTypeItemHolder.getInstance();
-		
-		ActivityRateTypeItem targetItem = dataHolder.getItem(player.getUserId(), activityRateTypeEnum);//已在之前生成数据的活动
-		
-		return targetItem!=null && !targetItem.isClosed();
-	}
-	
-	public float getRate(ActivityRateTypeEnum activityRateTypeEnum){
-		ActivityRateTypeCfg cfgById = ActivityRateTypeCfgDAO.getInstance().getCfgById(activityRateTypeEnum.getCfgId());
-		return cfgById==null? 1 : cfgById.getRate();
+		if(activityRateTypeEnum != null){
+			ActivityRateTypeItem targetItem = dataHolder.getItem(player.getUserId(), activityRateTypeEnum);//已在之前生成数据的活动		
+			return targetItem!=null && !targetItem.isClosed();
+		}else{
+			GameLog.error("activityratetypemgr", "enmu为空", "没有找到对应活动类型");
+			return false;
+		}
 	}
 
+	public float getRate(ActivityRateTypeEnum activityRateTypeEnum) {
+		ActivityRateTypeCfg cfgById = ActivityRateTypeCfgDAO.getInstance()
+				.getCfgById(activityRateTypeEnum.getCfgId());
+		return cfgById == null ? 1 : cfgById.getRate();
+	}
 	
 	/**登陆或打开活动入口时，核实所有活动是否开启，并根据活动类型生成空的奖励数据;如果活动为重复的,如何在活动重复时晴空*/
 	public void checkActivityOpen(Player player) {
@@ -46,29 +49,38 @@ public class ActivityRateTypeMgr {
 	}
 
 	private void checkNewOpen(Player player) {
-		ActivityRateTypeItemHolder dataHolder = ActivityRateTypeItemHolder.getInstance();
-		List<ActivityRateTypeCfg> allCfgList = ActivityRateTypeCfgDAO.getInstance().getAllCfg();
-		for (ActivityRateTypeCfg activityRateTypeCfg : allCfgList) {//遍历种类*各类奖励数次数,生成开启的种类个数空数据
-			if(isOpen(activityRateTypeCfg)){
-				ActivityRateTypeEnum typeEnum = ActivityRateTypeEnum.getById(activityRateTypeCfg.getId());
-				if(typeEnum != null){
-					ActivityRateTypeItem targetItem = dataHolder.getItem(player.getUserId(), typeEnum);//已在之前生成数据的活动
-					
-					if(targetItem == null){
-						targetItem = ActivityRateTypeCfgDAO.getInstance().newItem(player, typeEnum);//生成新开启活动的数据
-						if(targetItem!=null){
-							dataHolder.addItem(player, targetItem);
-						}
-					}else{
-						if(!StringUtils.equals(targetItem.getVersion(), activityRateTypeCfg.getVersion())){
-							targetItem.setVersion(activityRateTypeCfg.getVersion());
-							targetItem.setClosed(false);
-							dataHolder.updateItem(player, targetItem);						}
-					}
-				}
-				
-				
+		ActivityRateTypeItemHolder dataHolder = ActivityRateTypeItemHolder
+				.getInstance();
+		List<ActivityRateTypeCfg> allCfgList = ActivityRateTypeCfgDAO
+				.getInstance().getAllCfg();
+		for (ActivityRateTypeCfg activityRateTypeCfg : allCfgList) {// 遍历种类*各类奖励数次数,生成开启的种类个数空数据
+			if (!isOpen(activityRateTypeCfg)) {
+				//活动未开启
+				continue;
 			}
+			ActivityRateTypeEnum typeEnum = ActivityRateTypeEnum
+					.getById(activityRateTypeCfg.getId());
+			if (typeEnum == null) {
+				//枚举没有配置
+				continue;
+			}
+			ActivityRateTypeItem targetItem = dataHolder.getItem(
+					player.getUserId(), typeEnum);// 已在之前生成数据的活动
+			if (targetItem == null) {
+				targetItem = ActivityRateTypeCfgDAO.getInstance().newItem(
+						player, typeEnum);// 生成新开启活动的数据
+				if (targetItem != null) {
+					dataHolder.addItem(player, targetItem);
+				}
+			} else {
+				if (!StringUtils.equals(targetItem.getVersion(),
+						activityRateTypeCfg.getVersion())) {
+					targetItem.setVersion(activityRateTypeCfg.getVersion());
+					targetItem.setClosed(false);
+					dataHolder.updateItem(player, targetItem);
+				}
+			}
+
 		}
 	}
 	private void checkClose(Player player) {
