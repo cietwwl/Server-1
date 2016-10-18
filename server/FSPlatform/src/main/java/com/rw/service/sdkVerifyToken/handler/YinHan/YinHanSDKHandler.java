9d779c529b87cf4d41b30933f0cb9b21de40ffdd
lida <lida@rainwings.cn>
2016-10-18 19:27:05 +0800

    dev:新的ios sdk接入

---
+++ server/FSPlatform/src/main/java/com/rw/service/sdkVerifyToken/handler/YinHan/YinHanSDKHandler.java
@@ -0,0 +1,12 @@
+package com.rw.service.sdkVerifyToken.handler.YinHan;
+
+/**
+ * ios正式的二次验证地址
+ * @author lida
+ *
+ */
+public class YinHanSDKHandler extends AbsSDKHandler{
+
+	protected String URL = "http://nsdk.yhres.cn:4003/verifyToken";
+	protected String BackupURL = "http://123.206.2.107:4003/verifyToken";
+}
