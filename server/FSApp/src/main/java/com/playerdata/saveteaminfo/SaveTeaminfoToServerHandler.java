09de86b9a062325a8577f7fcea88154bb908990e
perrychen <perrychen@rainwings.cn>
2016-10-18 14:41:30 +0800

    1、EmbattleInfo添加对客户端数据进行校验的代码。如果客户端传过来的数据时完全错误的情况下，会默认给一个主角到0号位置 2、GCompDetailInfoMgr恢复一段之前被错误注释的代码

--- server/FSApp/src/main/java/com/playerdata/saveteaminfo/SaveTeaminfoToServerHandler.java
+++ server/FSApp/src/main/java/com/playerdata/saveteaminfo/SaveTeaminfoToServerHandler.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.google.protobuf.ByteString;
 import com.playerdata.Player;
+import com.playerdata.embattle.EmbattleHeroPosition;
 import com.playerdata.embattle.EmbattleInfoMgr;
 import com.playerdata.embattle.EmbattlePositonHelper;
 import com.playerdata.hero.core.FSHeroMgr;
@@ -34,8 +35,18 @@ public class SaveTeaminfoToServerHandler {
 		String str = msgMSRequest.getRecordkey();
 		List<BattleHeroPosition> positionList = msgMSRequest.getBattleHeroPositionList();
 
+		List<EmbattleHeroPosition> parseList = EmbattlePositonHelper.parseMsgHeroPos2Memery(positionList);
+		
+		if(parseList.isEmpty()) {
+			// 2016-10-18 by Perry：empty肯定是一个错误的数据，为了保持这个数据的正确性，只能把主角放进去
+			EmbattleHeroPosition pos = new EmbattleHeroPosition();
+			pos.setId(player.getUserId());
+			pos.setPos(0);
+			parseList.add(pos);
+		}
+		
 		// 存储到阵容中
-		EmbattleInfoMgr.getMgr().updateOrAddEmbattleInfo(player, type.getNumber(), str, EmbattlePositonHelper.parseMsgHeroPos2Memery(positionList));
+		EmbattleInfoMgr.getMgr().updateOrAddEmbattleInfo(player, type.getNumber(), str, parseList);
 
 		// 通知阵容发生了改变
 		FSHeroMgr.getInstance().updateFightingTeamWhenEmBattleChange(player.getUserId());
