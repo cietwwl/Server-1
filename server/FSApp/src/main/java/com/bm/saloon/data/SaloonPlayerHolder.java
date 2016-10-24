eea30bb3a5321fe9313f4e0d839739a40ee6e27f
allen <allen@rainwings.cn>
2016-10-24 17:32:58 +0800

    dev: 临时提交

--- server/FSApp/src/main/java/com/bm/saloon/data/SaloonPlayerHolder.java
+++ server/FSApp/src/main/java/com/bm/saloon/data/SaloonPlayerHolder.java
@@ -23,13 +23,10 @@ public class SaloonPlayerHolder{
 	public void synAddData(Player player, SaloonPlayer addItem){
 		ClientDataSynMgr.updateData(player, addItem, synType,eSynOpType.ADD_SINGLE);
 	}
-	public void synRemoveData(Player player, String removeId){
-		SaloonPlayer removeItem = SaloonPlayer.newInstance(removeId);
+	public void synRemoveData(Player player, SaloonPlayer removeItem){
+		
 		ClientDataSynMgr.updateData(player, removeItem, synType,eSynOpType.REMOVE_SINGLE);
 	}
-//	public void synUpdateData(Player player, SaloonPlayer addItem){
-//		ClientDataSynMgr.updateData(player, addItem, synType,eSynOpType.UPDATE_SINGLE);
-//	}
 
 	
 	
