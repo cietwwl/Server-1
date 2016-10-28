e0724e0e85c8c2d30245b592c1ea5c77e26e77be
perrychen <perrychen@rainwings.cn>
2016-10-28 21:34:45 +0800

    修复一处被去除的API调用

--- server/FSApp/src/main/java/com/playerdata/activity/growthFund/GrowthFundBoughtCountSynTask.java
+++ server/FSApp/src/main/java/com/playerdata/activity/growthFund/GrowthFundBoughtCountSynTask.java
@@ -3,6 +3,7 @@ package com.playerdata.activity.growthFund;
 import java.util.List;
 import java.util.concurrent.RejectedExecutionException;
 
+import com.playerdata.Player;
 import com.playerdata.PlayerMgr;
 import com.rwbase.common.timer.IGameTimerTask;
 import com.rwbase.common.timer.core.FSGameTimeSignal;
@@ -29,7 +30,10 @@ public class GrowthFundBoughtCountSynTask implements IGameTimerTask {
 			builder.setReqType(EGrowthFundRequestType.NOTIFY_BOUGHT_COUNT_CHANGE);
 			builder.setResultType(EGrowthFundResultType.SUCCESS);
 			GrowthFundResponse response = builder.build();
-			PlayerMgr.getInstance().sendPlayerAll(Command.MSG_BUY_GROWTH_FUND	, response.toByteString());
+			List<Player> allPlayers = PlayerMgr.getInstance().getOnlinePlayers();
+			for (int i = 0, size = allPlayers.size(); i < size; i++) {
+				allPlayers.get(i).SendMsg(Command.MSG_BUY_GROWTH_FUND, response.toByteString());
+			}
 		}
 		return "SUCCESS";
 	}
