d78ed2d0bdc1c9f870912d01250b59a0d563ca1f
lida <lida@rainwings.cn>
2016-10-31 19:48:46 +0800

    dev:ios判断32位和64位下载lua资源

--- server/FSVerWeb/src/main/java/com/fy/lua/validate/LuaRequestType.java
+++ server/FSVerWeb/src/main/java/com/fy/lua/validate/LuaRequestType.java
@@ -1,6 +1,6 @@
 package com.fy.lua.validate;
 
 public class LuaRequestType {
-	public static final int DOWNLOAD_LUAFILE = 1;   //请求下载lua files文件
-	public static final int DOWNLOAD_LUAZIP = 2;	 //请求下载lua压缩文件
+	public static final int DOWNLOAD_LUAFILE = 1;	   	//请求下载lua files文件
+	public static final int DOWNLOAD_LUAZIP = 2;		//请求下载lua压缩文件
 }
