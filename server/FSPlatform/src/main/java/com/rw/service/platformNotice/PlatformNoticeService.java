4620c0368c57dca22ab3a38cea3fd5d94d70a65a
lida <lida@rainwings.cn>
2016-11-20 22:43:11 +0800

    dev:修改公告

--- server/FSPlatform/src/main/java/com/rw/service/platformNotice/PlatformNoticeService.java
+++ server/FSPlatform/src/main/java/com/rw/service/platformNotice/PlatformNoticeService.java
@@ -11,18 +11,12 @@ public class PlatformNoticeService  implements RequestService{
 	@Override
 	public ByteString doTask(Request request, Account account) {
 		// TODO Auto-generated method stub
-		
+
 		ByteString result = null;
 		try{
 			NoticeRequest noticeReqest = NoticeRequest.parseFrom(request.getBody().getSerializedContent());
-			switch (noticeReqest.getType()) {
-			case PlatformNotice:
-				result = PlatformNoticeHandler.getInstance().requestNotice(noticeReqest, account);
-				break;
-			default:
-				break;
-			}
-		}catch(Exception ex){
+			result = PlatformNoticeHandler.getInstance().requestNotice(noticeReqest, account);
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 		return result;
