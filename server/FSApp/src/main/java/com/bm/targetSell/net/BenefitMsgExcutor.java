fe9c26479d4e3608536a9a8fedc22d944d068b9c
Alex <alexzhao@rainwings.cn>
2016-11-20 16:37:10 +0800

    临时提交

---
+++ server/FSApp/src/main/java/com/bm/targetSell/net/BenefitMsgExcutor.java
@@ -0,0 +1,35 @@
+package com.bm.targetSell.net;
+
+import com.rw.fsutil.remote.parse.FSMessageExecutor;
+import com.rwbase.gameworld.GameWorldFactory;
+
+public class BenefitMsgExcutor implements FSMessageExecutor<String>{
+
+	@Override
+	public void execute(String message) {
+		
+		GameWorldFactory.getGameWorld().asynExecute(new ResponseTask(message));
+	}
+	
+	
+	private class ResponseTask implements Runnable{
+
+		private String content;
+		
+		
+		
+		public ResponseTask(String content) {
+			this.content = content;
+		}
+
+
+
+		@Override
+		public void run() {
+			System.out.println("recv response:" + content);
+			BenefitSystemMsgService.getHandler().doTask(content);
+		}
+		
+	}
+
+}
--- server/FSUtil/src/main/java/com/rw/fsutil/remote/RemoteMessageDecoder.java
+++ server/FSUtil/src/main/java/com/rw/fsutil/remote/RemoteMessageDecoder.java
@@ -1,5 +1,6 @@
 package com.rw.fsutil.remote;
 
+import java.util.Arrays;
 import java.util.List;
 
 import com.rw.fsutil.remote.parse.FSMessageDecoder;
@@ -34,6 +35,9 @@ public class RemoteMessageDecoder<ReceiveMessage> extends ByteToMessageDecoder {
 		in.readBytes(prefixDatas);
 		int dataSize = prefix.getDataSize(prefixDatas);
 		if (dataSize < 0 || dataSize > maxDataSize) {
+			byte[] data = new byte[in.readableBytes()];
+			in.readBytes(data);
+			System.out.println("error:"+new String(data));
 			throw new CorruptedFrameException("wrong size of packet ,size=" + dataSize);
 		}
 		if (in.readableBytes() < dataSize) {
@@ -42,8 +46,32 @@ public class RemoteMessageDecoder<ReceiveMessage> extends ByteToMessageDecoder {
 		}
 		byte[] data = new byte[dataSize];
 		in.readBytes(data);
+		String content = new String(data);
+		System.out.println("normal:"+content);
+		if(content.charAt(content.length()-1) != '}'){
+
+			int readSize = in.readableBytes();
+			System.out.println("exception:"+readSize+",dataSize="+dataSize);
+			if(readSize > 0){
+				in.markReaderIndex();
+				byte[] remainArray = new byte[readSize];
+				in.readBytes(remainArray);
+				in.resetReaderIndex();
+				byte[] total = new byte[dataSize+readSize];
+				System.arraycopy(data, 0, total, 0, data.length);
+				System.arraycopy(remainArray, 0, total, data.length, remainArray.length);
+				System.out.println("exception:"+new String(total));
+			}
+		}
 		ReceiveMessage message = decoder.convertToMessage(data);
 		out.add(message);
+		int remainSize = in.readableBytes();
+		if(remainSize == 0){
+			return;
+		}
+		in.markReaderIndex();
+		byte[] remain = new byte[remainSize];
+		System.out.println("remain:"+new String(remain));
+		in.resetReaderIndex();
 	}
-
 }
