1a2f62c097986e08ff84a4a0f5497ba633e2a4f7
akenwang <akenwang@rainwings.cn>
2016-11-25 18:46:55 +0800

    顺序测试副本战斗的机器人

--- server/RobotPressureTest/src/main/java/com/rw/handler/copy/data/CopyHolder.java
+++ server/RobotPressureTest/src/main/java/com/rw/handler/copy/data/CopyHolder.java
@@ -3,6 +3,8 @@ package com.rw.handler.copy.data;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.config.copyCfg.CopyCfg;
+import com.config.copyCfg.CopyCfgDAO;
 import com.rw.dataSyn.SynDataListHolder;
 import com.rwproto.DataSynProtos.MsgDataSyn;
 
@@ -11,13 +13,33 @@ public class CopyHolder{
 	private Map<Integer, Integer> copyTime = new HashMap<Integer, Integer>();
 	private SynDataListHolder<CopyLevelRecord> listHolder = new SynDataListHolder<CopyLevelRecord>(CopyLevelRecord.class);
 	
+	private int currentNormalCopyId = 0;
+	private int currentEliteCopyId = 0;
+	private int fightingCopyId = 0;
+	
 	public CopyHolder() { }
 	
 	public void syn(MsgDataSyn msgDataSyn) {
 		listHolder.Syn(msgDataSyn);
-		int i = 0;
-		i++;
-		System.out.println(i);		
+		for(CopyLevelRecord record : listHolder.getItemList()){
+			if(record.getPassStar() <= 0) continue;
+			CopyCfg cfg = CopyCfgDAO.getInstance().getCfgById(String.valueOf(record.getLevelId()));
+			if(cfg == null){
+				System.out.println("CopyCfg配置文件不一致[" + record.getLevelId() + "]");
+				continue;
+			}
+			if(cfg.getLevelType() == 0){
+				if(record.getLevelId() > currentNormalCopyId){
+					currentNormalCopyId = record.getLevelId();
+					System.out.println("currentNormalCopyId=====================>>" + currentNormalCopyId);
+				}
+			}else if(cfg.getLevelType() == 1){
+				if(record.getLevelId() > currentEliteCopyId){
+					currentEliteCopyId = record.getLevelId();
+					System.out.println("currentEliteCopyId=====================>>" + currentEliteCopyId);
+				}
+			}
+		}
 	}
 
 	public Map<Integer, Integer> getCopyTime() {
@@ -26,5 +48,21 @@ public class CopyHolder{
 
 	public void setCopyTime(Map<Integer, Integer> copyTime) {
 		this.copyTime = copyTime;
+	}
+
+	public int getCurrentNormalCopyId() {
+		return currentNormalCopyId;
+	}
+
+	public int getCurrentEliteCopyId() {
+		return currentEliteCopyId;
+	}
+
+	public int getFightingCopyId() {
+		return fightingCopyId;
+	}
+
+	public void setFightingCopyId(int fightingCopyId) {
+		this.fightingCopyId = fightingCopyId;
 	}
 }
