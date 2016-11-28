ac9e4dac0da9390c1ee151474be6ed2a09b34399
Alex <alexzhao@rainwings.cn>
2016-11-28 15:40:52 +0800

    精准营销添加领取邮件逻辑
    # Conflicts:
    #	server/FSApp/src/main/resources/config/email/emailCfg.csv

--- server/FSApp/src/main/java/com/bm/targetSell/net/BenefitMsgController.java
+++ server/FSApp/src/main/java/com/bm/targetSell/net/BenefitMsgController.java
@@ -40,7 +40,7 @@ public class BenefitMsgController {
 	}
 
 	public void init(String removeIp, int port, int localPort, int timeoutMillis, int priod) {
-		server = RemoteMessageServiceFactory.createService(RemoteMessageEnum.RMType_Benefit.getId(), removeIp,port, 2, 2, decoder, encoder, executor);
+		server = RemoteMessageServiceFactory.createService(RemoteMessageEnum.RMType_Benefit.getId(), removeIp,port, 1, 1, decoder, encoder, executor);
 		FSGameTimerMgr.getInstance().submitSecondTask(new HeartBeatTask(priod), priod);
 	}
 
