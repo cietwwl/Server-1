f540621e7a19fdb687f3fb43609a845deb470f41
Alex <alexzhao@rainwings.cn>
2016-11-29 10:44:14 +0800

    修改世界boss复活计时逻辑

--- server/FSApp/src/main/java/com/bm/worldBoss/WBUserMgr.java
+++ server/FSApp/src/main/java/com/bm/worldBoss/WBUserMgr.java
@@ -26,7 +26,7 @@ public class WBUserMgr {
 		WBUserData wbUserData = WBUserDataHolder.getInstance().get(player.getUserId());
 		int bossVersion = wbUserData.getBossVersion();
 		if(!WBMgr.getInstance().isSameBoss(bossVersion)){
-			wbUserData.clean(bossVersion);
+			//wbUserData.clean(bossVersion);  不可以在同步的时候检查并清除上一次记录，否则下一个boss在准备期就看不到上一个boss战的记录
 			WBUserDataHolder.getInstance().update(player);
 		}else{
 			WBUserDataHolder.getInstance().syn(player, wbUserDataVersion);		
