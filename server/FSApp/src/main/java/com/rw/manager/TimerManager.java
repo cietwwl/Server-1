f9df0ce6954797c4fb2b6ed3ac24e4eaa509b04b
Jamaz <jamazruan@rainwings.cn>
2016-12-14 11:46:40 +0800

    bugfix:屏蔽测试代码

--- server/FSApp/src/main/java/com/rw/manager/TimerManager.java
+++ server/FSApp/src/main/java/com/rw/manager/TimerManager.java
@@ -7,7 +7,6 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import com.bm.group.GroupBM;
-import com.bm.saloon.SaloonBmFactory;
 import com.bm.worldBoss.state.WBStateFSM;
 import com.gm.activity.RankingActivity;
 import com.log.GameLog;
@@ -15,7 +14,6 @@ import com.log.LogModule;
 import com.playerdata.PlayerMgr;
 import com.playerdata.RankingMgr;
 import com.playerdata.activityCommon.ActivityDetector;
-import com.playerdata.dataSyn.sameSceneSyn.DataAutoSynMgr;
 import com.playerdata.groupFightOnline.state.GFightStateTransfer;
 import com.rw.fsutil.common.SimpleThreadFactory;
 import com.rw.netty.UserChannelMgr;
@@ -32,7 +30,6 @@ public class TimerManager {
 	private static TimeSpanOpHelper biTime10MinuteOp;
 
 	private static TimeSpanOpHelper timeMinuteOp;
-	// private static TimeSpanOpHelper time5MinuteOp;
 	private static TimeSpanOpHelper timeHourOp;
 	private static DayOpOnHour dayOpOnZero;
 	private static DayOpOnHour dayOpOn5Am;
@@ -40,7 +37,6 @@ public class TimerManager {
 	private static DayOpOnHour dayOpOn23h50m4Bilog;
 	private static TimeSpanOpHelper timeSecondOp;// 秒时效
 	private static TimeSpanOpHelper time20SecondOp;// 20秒时效
-	private static TimeSpanOpHelper time10MicroSecondOp; // 10毫秒时效
 
 	private static ScheduledExecutorService timeService = Executors.newScheduledThreadPool(1, new SimpleThreadFactory("timer"));
 	private static ScheduledExecutorService biTimeService = Executors.newScheduledThreadPool(1, new SimpleThreadFactory("biTimer"));
@@ -51,20 +47,6 @@ public class TimerManager {
 		final long MINUTE = 60 * SECOND;
 		final long MINUTE_10 = 10 * MINUTE;
 		final long HOUR = 60 * MINUTE;
-		final long MICROSECOND = 30; // 30毫秒
-
-		time10MicroSecondOp = new TimeSpanOpHelper(new ITimeOp() {
-
-			@Override
-			public void doTask() {
-				try{
-					SaloonBmFactory.getInstance().update();
-					DataAutoSynMgr.getInstance().synDataAuto();
-				} catch (Exception ex) {
-
-				}
-			}
-		}, MICROSECOND);
 
 		timeSecondOp = new TimeSpanOpHelper(new ITimeOp() {
 
@@ -170,21 +152,8 @@ public class TimerManager {
 			@Override
 			public void run() {
 				try {
-					time10MicroSecondOp.tryRun();
-				} catch (Throwable e) {
-					GameLog.error("同屏测试", "TimerManager", "TimerManager[init]同屏测试数据错误", e);
-				}
-			}
-		}, 0, 10, TimeUnit.MICROSECONDS);
-
-		timeService.scheduleAtFixedRate(new Runnable() {
-
-			@Override
-			public void run() {
-				try {
 					timeSecondOp.tryRun();
 					time20SecondOp.tryRun();
-					time10MicroSecondOp.tryRun();
 				} catch (Throwable e) {
 					GameLog.error(LogModule.COMMON.getName(), "TimerManager", "TimerManager[init]用户数据保存错误", e);
 				}
