687e788312547875c15cd394051baf92e2fc274c
huchong <huchong@rainwings.cn>
2016-12-29 10:36:40 +0800

    dev：帮派祈福机器人开发

--- server/RobotPressureTest/src/main/java/com/rw/handler/group/GroupPrayHandler.java
+++ server/RobotPressureTest/src/main/java/com/rw/handler/group/GroupPrayHandler.java
@@ -73,10 +73,10 @@ public class GroupPrayHandler implements RandomMethodIF {
 		req.setReqType(ReqType.SEND_PRAY);
 
 		SendPrayReqMsg.Builder sendPrayReq = SendPrayReqMsg.newBuilder();
-		String randomPrayUserId = client.getGroupPrayData().randomPrayUserId();
-		System.err.println("随机到的帮派成员Id---->" + randomPrayUserId);
-		// sendPrayReq.setMemberId(randomPrayUserId);
-		sendPrayReq.setMemberId("600100000018");
+		sendPrayReq.setMemberId(client.getGroupPrayData().randomPrayUserId());
+		// String randomPrayUserId = client.getGroupPrayData().randomPrayUserId();
+		// System.err.println("随机到的帮派成员Id---->" + randomPrayUserId);
+		// sendPrayReq.setMemberId("600100000018");
 		req.setSendPrayReq(sendPrayReq);
 
 		return client.getMsgHandler().sendMsg(Command.MSG_GROUP_PRAY, req.build().toByteString(), sendPrayMsgReceiver);
