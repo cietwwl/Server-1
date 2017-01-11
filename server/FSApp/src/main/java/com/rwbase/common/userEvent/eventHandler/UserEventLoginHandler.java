8badb7326babcf5c36472c80ee3d2820dfc0b09a
akenwang <akenwang@rainwings.cn>
2017-01-11 08:51:17 +0800

    累积登录活动的次数统计问题再次修改

--- server/FSApp/src/main/java/com/rwbase/common/userEvent/eventHandler/UserEventLoginHandler.java
+++ server/FSApp/src/main/java/com/rwbase/common/userEvent/eventHandler/UserEventLoginHandler.java
@@ -3,10 +3,7 @@ package com.rwbase.common.userEvent.eventHandler;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
-
 import com.bm.serverStatus.ServerStatusMgr;
-import com.common.HPCUtil;
 import com.log.GameLog;
 import com.log.LogModule;
 import com.playerdata.Player;
@@ -35,16 +32,7 @@ public class UserEventLoginHandler implements IUserEventHandler {
 				if(!ActivityDetector.getInstance().containsActivityByActId(ActivityTypeFactory.CountType, ActivityCountTypeEnum.Login.getCfgId())){
 					return;
 				}
-				/** 登陆是否隔天;如果不加between则必须保证dataitem会在结束时立刻移出 */
-				boolean isnewday = false;
-				if (StringUtils.equals(params + "", "0")) {// 没有活动的登陆数据，首次登陆
-					isnewday = true;
-				} else {
-					isnewday = HPCUtil.isResetTime(Long.parseLong(params.toString()));
-				}
-				if (isnewday) {
-					ActivityCountTypeMgr.getInstance().addCount(player, ActivityCountTypeEnum.Login, 1);
-				}
+				ActivityCountTypeMgr.getInstance().addCount(player, ActivityCountTypeEnum.Login, 1);
 			}
 
 			@Override
@@ -67,5 +55,4 @@ public class UserEventLoginHandler implements IUserEventHandler {
 		}
 
 	}
-
 }
